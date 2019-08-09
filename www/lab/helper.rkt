30c1d52982cebcfe90b993e569dd89ebab19f73c
David Van Horn <dvanhorn@cs.umd.edu>
2019-08-08 22:08:25 -0400

    GC more irrelevant files.

--- www/lab/helper.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket
-(require scribble/manual scribble/core)
-(provide (all-defined-out))
-
-(define (colorize c . content)
-  (elem #:style (style #f (list (color-property c)))
-        content))
