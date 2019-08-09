461b078d833abd4366b5ebff286ec8a4c4b8a016
David Van Horn <dvanhorn@cs.umd.edu>
2019-08-08 22:27:22 -0400

    Staff.

--- www/defns.rkt
+++ www/defns.rkt
@@ -1,10 +1,12 @@
 #lang racket
 (provide (all-defined-out))
-(require scribble/core scribble/html-properties)
+(require scribble/core scribble/html-properties scribble/manual) 
 
 (define semester "fall")
 (define year "2019")
 (define courseno "CMSC 430")
+
+(define IRB (link "https://maps.google.com/maps?q=BRENDAN+IRIBE+CENTER%2c+College+Park%2c+MD&z=18" "IRB"))
 
 #|
 (define m1-date "Oct 1")
