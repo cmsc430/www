07fb1af5e1c654c0658fae8fc516c782868cdf4c
David Van Horn <dvanhorn@cs.umd.edu>
2019-08-20 08:41:57 +0200

    Trying to avoid the 'entering/leaving directory' noise.

--- www/notes/blackmail/asm/interp.rkt
+++ www/notes/blackmail/asm/interp.rkt
@@ -13,7 +13,7 @@
       #:exists 'truncate
       (λ ()
         (asm-display a)))
-    (system (format "make -s -C ~a ~a" dir t.run))
+    (system (format "(cd ~a && make -s ~a)" dir t.run))
     (delete-file t.s)
     (with-input-from-string
         (with-output-to-string
