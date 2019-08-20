e2f1ed685e1aab944475ef7604a326e803478d16
David Van Horn <dvanhorn@cs.umd.edu>
2019-08-20 18:03:00 -0400

    Make a compile-file.rkt for Abscond.

--- www/notes/abscond/compile.rkt
+++ www/notes/abscond/compile.rkt
@@ -1,19 +1,8 @@
 #lang racket
 (provide (all-defined-out))
-(require "asm/printer.rkt")
 
 ;; Expr -> Asm
 (define (abscond-compile e)
   `(entry
     (mov rax ,e)
     ret))
-
-;; String -> Void
-;; Compile contents of given file name,
-;; emit asm code on stdout
-(define (main fn)
-  (with-input-from-file fn
-    (λ ()
-      (let ((p (read)))
-        (unless (integer? p) (error "syntax error" p))
-        (asm-display (abscond-compile p))))))
