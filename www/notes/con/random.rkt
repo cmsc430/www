249aced08e6577da9de2a3623fd669ea507ff4ba
David Van Horn <dvanhorn@cs.umd.edu>
2019-08-22 21:01:31 -0400

    Random testing compiler for Con.

---
+++ www/notes/con/random.rkt
@@ -0,0 +1,12 @@
+#lang racket
+(provide (all-defined-out))
+
+;; Randomly generate an expression
+(define (random-expr)
+  (contract-random-generate
+   (flat-rec-contract e
+                      (integer-in #f #f)
+                      (list/c 'add1 e)
+                      (list/c 'sub1 e)
+                      (list/c 'if (list/c 'zero? e) e e))))
+
