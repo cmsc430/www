fc5c4618954347bb2298ea320a83b0f7f550a4d9
David Van Horn <dvanhorn@cs.umd.edu>
2019-08-22 21:01:31 -0400

    Random testing compiler for Con.

---
+++ www/notes/con/test/compile-rand.rkt
@@ -0,0 +1,9 @@
+#lang racket
+(require "../interp.rkt" "../compile.rkt" "../asm/interp.rkt" "../random.rkt" rackunit)
+
+(define (check-compiler e)
+    (check-eqv? (interp e)
+                (asm-interp (compile e))))
+
+(for ([i (in-range 500)])
+  (check-compiler (random-expr)))
