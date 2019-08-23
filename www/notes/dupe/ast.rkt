68871348f068816daaf1a08fdf93e138deb00ba3
David Van Horn <dvanhorn@cs.umd.edu>
2019-08-22 22:33:21 -0400

    Fix up Dupe AST defn.

--- www/notes/dupe/ast.rkt
+++ www/notes/dupe/ast.rkt
@@ -1,24 +1,13 @@
 #lang racket
 ;; type Expr =
 ;; | Integer
-;; | Variable
+;; | Boolean
 ;; | `(add1 ,Expr)
 ;; | `(sub1 ,Expr)
-;; | `(,Prim2 ,Expr ,Expr)
-;; | `(if (zero? ,Expr) ,Expr ,Expr)
-;; | `(let ((,Variable ,Expr)) ,Expr)
+;; | `(zero? ,Expr)
+;; | `(if ,Expr ,Expr ,Expr)
 
 ;; type Prim2 =
 ;; | '*
 ;; | '+
 ;; | '-
-
-;; type ANF =
-;; | Imm
-;; | `(add1 ,ANF)
-;; | `(sub1 ,ANF)
-;; | `(,Prim2 ,Imm ,Imm)
-;; | `(if (zero? ,ANF) ,ANF ,ANF)
-;; | `(let ((,Variable ,ANF)) ,ANF)
-
-;; type Variable = Symbol
