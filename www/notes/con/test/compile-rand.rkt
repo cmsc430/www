acb410897f3013f135c0f1df06d257d8aac6881b
David Van Horn <dvanhorn@cs.umd.edu>
2019-08-23 11:18:32 -0400

    Indentation.

--- www/notes/con/test/compile-rand.rkt
+++ www/notes/con/test/compile-rand.rkt
@@ -2,8 +2,8 @@
 (require "../interp.rkt" "../compile.rkt" "../asm/interp.rkt" "../random.rkt" rackunit)
 
 (define (check-compiler e)
-    (check-eqv? (interp e)
-                (asm-interp (compile e))))
+  (check-eqv? (interp e)
+              (asm-interp (compile e))))
 
 (for ([i (in-range 500)])
   (check-compiler (random-expr)))
