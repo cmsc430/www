5ff48e6c226232cf7def6abb4a796b45a8fb4532
David Van Horn <dvanhorn@cs.umd.edu>
2019-09-21 22:18:39 -0400

    Update run-time.

--- www/notes/grift/compile.rkt
+++ www/notes/grift/compile.rkt
@@ -1,6 +1,5 @@
 #lang racket
 (provide (all-defined-out))
-(require "compile/help.rkt")
 
 ;; type CEnv = [Listof Variable]
 
@@ -18,55 +17,57 @@
 (define (compile-e e c)
   (match e
     [(? integer? i)
-     `((mov rax ,(arithmetic-shift i fixnum-shift)))]
+     `((mov rax ,(* i 2)))]
     [(? boolean? b)
-     `((mov rax ,(if b true-rep false-rep)))]
+     `((mov rax ,(if b #b11 #b01)))]
     [`(add1 ,e0)
      (let ((c0 (compile-e e0 c)))
        `(,@c0
          ,@assert-integer
-         (add rax ,(arithmetic-shift 1 fixnum-shift))))]
+         (add rax 2)))]
     [`(sub1 ,e0)
      (let ((c0 (compile-e e0 c)))
        `(,@c0
          ,@assert-integer
-         (sub rax ,(arithmetic-shift 1 fixnum-shift))))]             
+         (sub rax 2)))]
     [`(zero? ,e0)
-     (let ((c0 (compile-e e0 c)))
+     (let ((c0 (compile-e e0 c))
+           (l0 (gensym))
+           (l1 (gensym)))
        `(,@c0
          ,@assert-integer
          (cmp rax 0)
-         (sete al)
-         (movzx rax al)
-         (sal rax ,bool-shift)
-         (or eax ,false-rep)))]         
+         (mov rax #b01) ; #f
+         (jne ,l0)
+         (mov rax #b11) ; #t
+         ,l0))]
     [`(if ,e0 ,e1 ,e2)
      (let ((c0 (compile-e e0 c))
            (c1 (compile-e e1 c))
-           (c2 (compile-e e2 c)))
-       (match (gen-if-labels)
-         [(list if-f if-x)
-          `(,@c0
-            (cmp rax ,false-rep)
-            (je ,if-f)
-            ,@c1
-            (jmp ,if-x)
-            ,if-f
-            ,@c2
-            ,if-x)]))]   
+           (c2 (compile-e e2 c))
+           (l0 (gensym))
+           (l1 (gensym)))
+       `(,@c0
+         (cmp rax #b01) ; compare to #f
+         (je ,l0)       ; jump to c2 if #f
+         ,@c1
+         (jmp ,l1)      ; jump past c2
+         ,l0
+         ,@c2
+         ,l1))]
     [(? symbol? x)
      (let ((i (lookup x c)))
-       `((mov rax (offset rsp ,(- (add1 i))))))]    
+       `((mov rax (offset rsp ,(- (add1 i))))))]
     [`(let ((,x ,e0)) ,e1)
      (let ((c0 (compile-e e0 c))
            (c1 (compile-e e1 (cons x c))))
        `(,@c0
          (mov (offset rsp ,(- (add1 (length c)))) rax)
          ,@c1))]
+
     [`(+ ,e0 ,e1)
      (let ((c1 (compile-e e1 c))
            (c0 (compile-e e0 (cons #f c))))
-
        `(,@c1
          ,@assert-integer
          (mov (offset rsp ,(- (add1 (length c)))) rax)
@@ -77,7 +78,6 @@
     [`(- ,e0 ,e1)
      (let ((c1 (compile-e e1 c))
            (c0 (compile-e e0 (cons #f c))))
-
        `(,@c1
          ,@assert-integer
          (mov (offset rsp ,(- (add1 (length c)))) rax)
@@ -91,6 +91,12 @@
   (match cenv
     ['() (error "undefined variable:" x)]
     [(cons y cenv)
-     (match (symbol=? x y)
+     (match (eq? x y)
        [#t (length cenv)]
        [#f (lookup x cenv)])]))
+
+(define assert-integer
+  `((mov rbx rax)
+    (and rbx 1)
+    (cmp rbx 0)
+    (jne err)))
