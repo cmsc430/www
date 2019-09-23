41c5e9739e1724074f11d5208c42b6abe82cee0a
David Van Horn <dvanhorn@cs.umd.edu>
2019-09-23 14:38:39 -0400

    Refactor.

--- www/notes/hustle/compile.rkt
+++ www/notes/hustle/compile.rkt
@@ -22,6 +22,7 @@
 ;; end in #b000 and we tag with #b001 for boxes, etc.
 
 ;; type CEnv = (Listof (Maybe Variable))
+;; type Imm = Integer | Boolean | '()
 
 ;; Expr -> Asm
 (define (compile e)
@@ -39,112 +40,160 @@
 ;; Expr CEnv -> Asm
 (define (compile-e e c)
   (match e
-    [''() `((mov rax ,imm-type-empty))]
-    [`(box ,e0)
-     (let ((c0 (compile-e e0 c)))
-       `(,@c0
-         (mov (offset rdi 0) rax)
-         (mov rax rdi)
-         (or rax ,type-box)
-         (add rdi 8)))] ; allocate 8 bytes
-    [`(unbox ,e0)
-     (let ((c0 (compile-e e0 c)))
-       `(,@c0
-         ;; assert box
-         (xor rax ,type-box)
-         (mov rax (offset rax 0))))]
-    [`(cons ,e0 ,e1)
-     (let ((c0 (compile-e e0 c))
-           (c1 (compile-e e1 (cons #f c))))
-       `(,@c0
-         (mov (offset rsp ,(- (add1 (length c)))) rax)
-         ,@c1
-         (mov (offset rdi 0) rax)
-         (mov rax (offset rsp ,(- (add1 (length c)))))
-         (mov (offset rdi 1) rax)
-         (mov rax rdi)
-         (or rax ,type-pair)
-         (add rdi 16)))]
-    [`(car ,e0)
-     (let ((c0 (compile-e e0 c)))
-       `(,@c0
-         ;; assert pair
-         (xor rax ,type-pair)
-         (mov rax (offset rax 1))))]
-    [`(cdr ,e0)
-     (let ((c0 (compile-e e0 c)))
-       `(,@c0
-         ;; assert pair
-         (xor rax ,type-pair)
-         (mov rax (offset rax 0))))]               
-    [(? integer? i)
-     `((mov rax ,(arithmetic-shift i imm-shift)))]
-    [(? boolean? b)
-     `((mov rax ,(if b imm-type-true imm-type-false)))]
-    [`(add1 ,e0)
-     (let ((c0 (compile-e e0 c)))
-       `(,@c0
-         ,@assert-integer
-         (add rax ,(arithmetic-shift 1 imm-shift))))]
-    [`(sub1 ,e0)
-     (let ((c0 (compile-e e0 c)))
-       `(,@c0
-         ,@assert-integer
-         (sub rax ,(arithmetic-shift 1 imm-shift))))]
-    [`(zero? ,e0)
-     (let ((c0 (compile-e e0 c))
-           (l0 (gensym))
-           (l1 (gensym)))
-       `(,@c0
-         ,@assert-integer
-         (cmp rax 0)
-         (mov rax ,imm-type-false)
-         (jne ,l0)
-         (mov rax ,imm-type-true)
-         ,l0))]
-    [`(if ,e0 ,e1 ,e2)
-     (let ((c0 (compile-e e0 c))
-           (c1 (compile-e e1 c))
-           (c2 (compile-e e2 c))
-           (l0 (gensym))
-           (l1 (gensym)))
-       `(,@c0
-         (cmp rax ,imm-type-false)
-         (je ,l0)
-         ,@c1
-         (jmp ,l1)
-         ,l0
-         ,@c2
-         ,l1))]
-    [(? symbol? x)
-     (let ((i (lookup x c)))
-       `((mov rax (offset rsp ,(- (add1 i))))))]
-    [`(let ((,x ,e0)) ,e1)
-     (let ((c0 (compile-e e0 c))
-           (c1 (compile-e e1 (cons x c))))
-       `(,@c0
-         (mov (offset rsp ,(- (add1 (length c)))) rax)
-         ,@c1))]
+    [(? imm? i)            (compile-imm i)]
+    [(? symbol? x)         (compile-var x c)]    
+    [`(box ,e0)            (compile-box e0 c)]
+    [`(unbox ,e0)          (compile-unbox e0 c)]
+    [`(cons ,e0 ,e1)       (compile-cons e0 e1 c)]
+    [`(car ,e0)            (compile-car e0 c)]
+    [`(cdr ,e0)            (compile-cdr e0 c)]
+    [`(add1 ,e0)           (compile-add1 e0 c)]
+    [`(sub1 ,e0)           (compile-sub1 e0 c)]
+    [`(zero? ,e0)          (compile-zero? e0 c)]
+    [`(if ,e0 ,e1 ,e2)     (compile-if e0 e1 e2 c)]
+    [`(let ((,x ,e0)) ,e1) (compile-let x e0 e1 c)]
+    [`(+ ,e0 ,e1)          (compile-+ e0 e1 c)]
+    [`(- ,e0 ,e1)          (compile-- e0 e1 c)]))
 
-    [`(+ ,e0 ,e1)
-     (let ((c1 (compile-e e1 c))
-           (c0 (compile-e e0 (cons #f c))))
-       `(,@c1
-         ,@assert-integer
-         (mov (offset rsp ,(- (add1 (length c)))) rax)
-         ,@c0
-         ,@assert-integer
-         (add rax (offset rsp ,(- (add1 (length c)))))))]
+;; Any -> Boolean
+(define (imm? x)
+  (or (integer? x)
+      (boolean? x)
+      (equal? ''() x)))
 
-    [`(- ,e0 ,e1)
-     (let ((c1 (compile-e e1 c))
-           (c0 (compile-e e0 (cons #f c))))
-       `(,@c1
-         ,@assert-integer
-         (mov (offset rsp ,(- (add1 (length c)))) rax)
-         ,@c0
-         ,@assert-integer
-         (sub rax (offset rsp ,(- (add1 (length c)))))))]))
+;; Imm -> Asm
+(define (compile-imm i)
+  `((mov rax
+         ,(match i
+            [(? integer? i) (arithmetic-shift i imm-shift)]
+            [(? boolean? b) (if b imm-type-true imm-type-false)]
+            [''()           imm-type-empty]))))
+
+;; Variable CEnv -> Asm
+(define (compile-var x c)
+  (let ((i (lookup x c)))
+    `((mov rax (offset rsp ,(- (add1 i)))))))
+
+;; Expr CEnv -> Asm
+(define (compile-box e0 c)
+  (let ((c0 (compile-e e0 c)))
+    `(,@c0
+      (mov (offset rdi 0) rax)
+      (mov rax rdi)
+      (or rax ,type-box)
+      (add rdi 8)))) ; allocate 8 bytes
+
+;; Expr CEnv -> Asm
+(define (compile-unbox e0 c)
+  (let ((c0 (compile-e e0 c)))
+    `(,@c0
+      ,@assert-box
+      (xor rax ,type-box)
+      (mov rax (offset rax 0)))))
+
+;; Expr Expr CEnv -> Asm
+(define (compile-cons e0 e1 c)
+  (let ((c0 (compile-e e0 c))
+        (c1 (compile-e e1 (cons #f c))))
+    `(,@c0
+      (mov (offset rsp ,(- (add1 (length c)))) rax)
+      ,@c1
+      (mov (offset rdi 0) rax)
+      (mov rax (offset rsp ,(- (add1 (length c)))))
+      (mov (offset rdi 1) rax)
+      (mov rax rdi)
+      (or rax ,type-pair)
+      (add rdi 16))))
+
+;; Expr CEnv -> Asm
+(define (compile-car e0 c)
+  (let ((c0 (compile-e e0 c)))
+    `(,@c0
+      ,@assert-pair
+      (xor rax ,type-pair) ; untag
+      (mov rax (offset rax 1)))))
+
+;; Expr CEnv -> Asm
+(define (compile-cdr e0 c)
+  (let ((c0 (compile-e e0 c)))
+    `(,@c0
+      ,@assert-pair
+      (xor rax ,type-pair) ; untag
+      (mov rax (offset rax 0)))))   
+
+;; Expr CEnv -> Asm
+(define (compile-add1 e0 c)
+  (let ((c0 (compile-e e0 c)))
+    `(,@c0
+      ,@assert-integer
+      (add rax ,(arithmetic-shift 1 imm-shift)))))
+
+;; Expr CEnv -> Asm
+(define (compile-sub1 e0 c)
+  (let ((c0 (compile-e e0 c)))
+    `(,@c0
+      ,@assert-integer
+      (sub rax ,(arithmetic-shift 1 imm-shift)))))
+
+;; Expr CEnv -> Asm
+(define (compile-zero? e0 c)
+  (let ((c0 (compile-e e0 c))
+        (l0 (gensym))
+        (l1 (gensym)))
+    `(,@c0
+      ,@assert-integer
+      (cmp rax 0)
+      (mov rax ,imm-type-false)
+      (jne ,l0)
+      (mov rax ,imm-type-true)
+      ,l0)))
+
+;; Expr Expr Expr CEnv -> Asm
+(define (compile-if e0 e1 e2 c)
+  (let ((c0 (compile-e e0 c))
+        (c1 (compile-e e1 c))
+        (c2 (compile-e e2 c))
+        (l0 (gensym))
+        (l1 (gensym)))
+    `(,@c0
+      (cmp rax ,imm-type-false)
+      (je ,l0)
+      ,@c1
+      (jmp ,l1)
+      ,l0
+      ,@c2
+      ,l1)))
+
+;; Variable Expr Expr CEnv -> Asm
+(define (compile-let x e0 e1 c)
+  (let ((c0 (compile-e e0 c))
+        (c1 (compile-e e1 (cons x c))))
+    `(,@c0
+      (mov (offset rsp ,(- (add1 (length c)))) rax)
+      ,@c1)))
+
+;; Expr Expr CEnv -> Asm
+(define (compile-+ e0 e1 c) 
+  (let ((c1 (compile-e e1 c))
+        (c0 (compile-e e0 (cons #f c))))
+    `(,@c1
+      ,@assert-integer
+      (mov (offset rsp ,(- (add1 (length c)))) rax)
+      ,@c0
+      ,@assert-integer
+      (add rax (offset rsp ,(- (add1 (length c))))))))
+
+
+;; Expr Expr CEnv -> Asm
+(define (compile-- e0 e1 c)
+  (let ((c1 (compile-e e1 c))
+        (c0 (compile-e e0 (cons #f c))))
+    `(,@c1
+      ,@assert-integer
+      (mov (offset rsp ,(- (add1 (length c)))) rax)
+      ,@c0
+      ,@assert-integer
+      (sub rax (offset rsp ,(- (add1 (length c))))))))
 
 ;; code for "abc"
 '((mov (offset rdi 0)
@@ -170,8 +219,12 @@
        [#t (length cenv)]
        [#f (lookup x cenv)])]))
 
-(define assert-integer
+(define (assert-type mask type)
   `((mov rbx rax)
-    (and rbx ,imm-type-mask)
-    (cmp rbx 0)
+    (and rbx ,mask)
+    (cmp rbx ,type)
     (jne err)))
+
+(define assert-integer (assert-type imm-type-mask imm-type-int))
+(define assert-box     (assert-type result-type-mask type-box))
+(define assert-pair    (assert-type result-type-mask type-pair))
