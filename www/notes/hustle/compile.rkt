4590401dd1234cd860bc76f0d1e6b693636c9d1c
David Van Horn <dvanhorn@cs.umd.edu>
2019-09-23 13:44:14 -0400

    Slight representation change and clean-up of Hustle.

--- www/notes/hustle/compile.rkt
+++ www/notes/hustle/compile.rkt
@@ -1,24 +1,33 @@
 #lang racket
 (provide (all-defined-out))
 
-(define int-tag   #b0000) ;  0, 2, 4... integer
-(define box-tag   #b0001) ;  1 box
-(define pair-tag  #b0011) ;  3 pairs
-(define vect-tag  #b0101) ;  5 vectors
-(define str-tag   #b0111) ;  7 strings
-(define true-tag  #b1001) ;  9
-(define false-tag #b1011) ; 11
-(define empty-tag #b1101) ; 13
-(define char-tag  #b1111) ; 15
+;; An immediate is anything ending in #b0000
+;; All other tags in mask #b111 are pointers
+
+(define result-shift     3)
+(define result-type-mask (sub1 (arithmetic-shift 1 result-shift)))
+(define type-imm         #b000)
+(define type-box         #b001)
+(define type-pair        #b010)
+
+(define imm-shift        (+ 3 result-shift))
+(define imm-type-mask    (sub1 (arithmetic-shift 1 imm-shift)))
+(define imm-type-int     (arithmetic-shift #b000 result-shift))
+(define imm-type-true    (arithmetic-shift #b001 result-shift))
+(define imm-type-false   (arithmetic-shift #b010 result-shift))
+(define imm-type-empty   (arithmetic-shift #b011 result-shift))
+(define imm-type-char    (arithmetic-shift #b100 result-shift))
 
 ;; Allocate in 64-bit (8-byte) increments, so pointers
-;; end in #b000 and we tag with #b001 for pairs, etc.
+;; end in #b000 and we tag with #b001 for boxes, etc.
 
 ;; type CEnv = (Listof (Maybe Variable))
 
 ;; Expr -> Asm
 (define (compile e)
   `(entry
+    (sar rdi 4)  ;; align on quad boundary
+    (sal rdi 4)
     (add rdi 16)
     ,@(compile-e e '())
     ret
@@ -30,20 +39,19 @@
 ;; Expr CEnv -> Asm
 (define (compile-e e c)
   (match e
-    [''() `((mov rax ,empty-tag))]
+    [''() `((mov rax ,imm-type-empty))]
     [`(box ,e0)
      (let ((c0 (compile-e e0 c)))
        `(,@c0
          (mov (offset rdi 0) rax)
          (mov rax rdi)
-         (or rax ,box-tag)
-         (add rdi 8) ; bump by 8 bytes
-         ))]
+         (or rax ,type-box)
+         (add rdi 8)))] ; allocate 8 bytes
     [`(unbox ,e0)
      (let ((c0 (compile-e e0 c)))
        `(,@c0
          ;; assert box
-         (xor rax ,box-tag)
+         (xor rax ,type-box)
          (mov rax (offset rax 0))))]
     [`(cons ,e0 ,e1)
      (let ((c0 (compile-e e0 c))
@@ -55,34 +63,34 @@
          (mov rax (offset rsp ,(- (add1 (length c)))))
          (mov (offset rdi 1) rax)
          (mov rax rdi)
-         (or rax ,pair-tag)
+         (or rax ,type-pair)
          (add rdi 16)))]
     [`(car ,e0)
      (let ((c0 (compile-e e0 c)))
        `(,@c0
          ;; assert pair
-         (xor rax ,pair-tag)
+         (xor rax ,type-pair)
          (mov rax (offset rax 1))))]
     [`(cdr ,e0)
      (let ((c0 (compile-e e0 c)))
        `(,@c0
          ;; assert pair
-         (xor rax ,pair-tag)
+         (xor rax ,type-pair)
          (mov rax (offset rax 0))))]               
     [(? integer? i)
-     `((mov rax ,(* i 2)))]
+     `((mov rax ,(arithmetic-shift i imm-shift)))]
     [(? boolean? b)
-     `((mov rax ,(if b true-tag false-tag)))]
+     `((mov rax ,(if b imm-type-true imm-type-false)))]
     [`(add1 ,e0)
      (let ((c0 (compile-e e0 c)))
        `(,@c0
          ,@assert-integer
-         (add rax 2)))]
+         (add rax ,(arithmetic-shift 1 imm-shift))))]
     [`(sub1 ,e0)
      (let ((c0 (compile-e e0 c)))
        `(,@c0
          ,@assert-integer
-         (sub rax 2)))]
+         (sub rax ,(arithmetic-shift 1 imm-shift))))]
     [`(zero? ,e0)
      (let ((c0 (compile-e e0 c))
            (l0 (gensym))
@@ -90,9 +98,9 @@
        `(,@c0
          ,@assert-integer
          (cmp rax 0)
-         (mov rax ,false-tag) ; #f
+         (mov rax ,imm-type-false)
          (jne ,l0)
-         (mov rax ,true-tag)  ; #t
+         (mov rax ,imm-type-true)
          ,l0))]
     [`(if ,e0 ,e1 ,e2)
      (let ((c0 (compile-e e0 c))
@@ -101,10 +109,10 @@
            (l0 (gensym))
            (l1 (gensym)))
        `(,@c0
-         (cmp rax ,false-tag) ; compare to #f
-         (je ,l0)             ; jump to c2 if #f
+         (cmp rax ,imm-type-false)
+         (je ,l0)
          ,@c1
-         (jmp ,l1)            ; jump past c2
+         (jmp ,l1)
          ,l0
          ,@c2
          ,l1))]
@@ -138,6 +146,20 @@
          ,@assert-integer
          (sub rax (offset rsp ,(- (add1 (length c)))))))]))
 
+;; code for "abc"
+'((mov (offset rdi 0)
+       (integer->bits 3))
+  (mov (offset rdi 1)
+       (compile-char #\a))
+  (mov (offset rdi 1)
+       (char->bits #\b))
+  (mov (offset-rdi 2)
+       (char->bits #\c))
+  (mov rdi rax)
+  (or rax ,str-tag))
+  
+       
+
 
 ;; Variable CEnv -> Natural
 (define (lookup x cenv)
@@ -150,6 +172,6 @@
 
 (define assert-integer
   `((mov rbx rax)
-    (and rbx 1)
+    (and rbx ,imm-type-mask)
     (cmp rbx 0)
     (jne err)))
