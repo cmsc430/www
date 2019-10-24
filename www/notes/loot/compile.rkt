49f14364a993a12bd477888997d3cb2bbfed4611
David Van Horn <dvanhorn@cs.umd.edu>
2019-10-24 07:22:20 -0400

    Loot.

--- www/notes/loot/compile.rkt
+++ www/notes/loot/compile.rkt
@@ -1,4 +1,5 @@
 #lang racket
+(require "syntax.rkt")
 (provide (all-defined-out))
 
 ;; An immediate is anything ending in #b000
@@ -28,21 +29,23 @@
 ;; type CEnv = (Listof (Maybe Variable))
 ;; type Imm = Integer | Boolean | Char | ''()
 
+;; type LExpr =
+;; ....
+;; | `(λ ,Formals ,Label ,Expr)
+
+;; type Label = (quote Symbol)
 
 ;; Expr -> Asm
 (define (compile e)
   (let ((le (label-lambda e)))
-  `(entry
-    ,@(compile-tail-e le '())
-    ret
-
-    ,@(compile-lambda-definitions (lambdas le))
-    
-    err
-    (push rbp)
-    (call error)
-    ret)))
-
+    `(entry
+      ,@(compile-tail-e le '())
+      ret
+      ,@(compile-lambda-definitions (lambdas le))    
+      err
+      (push rbp)
+      (call error)
+      ret)))
 
 ;; (Listof Lambda) -> Asm
 (define (compile-lambda-definitions ls)
@@ -51,81 +54,11 @@
 ;; Lambda -> Asm
 (define (compile-lambda-definition l)
   (match l
-    [`(λ ,xs ',label ,e)
-     (compile-define label (append xs (fvs/unique l)) e)]))
-
-;; type LExpr =
-;; ....
-;; | `(λ ,Formals ,Label ,Expr)
-
-;; type Label = (quote Symbol)
-
-;; Expr -> LExpr
-(define (label-lambda e)
-  (match e
-    [(? symbol? x)         x]
-    [(? imm? i)            i]
-    [`(box ,e0)            `(box ,(label-lambda e0))]
-    [`(unbox ,e0)          `(unbox ,(label-lambda e0))]
-    [`(cons ,e0 ,e1)       `(cons ,(label-lambda e0) ,(label-lambda e1))]
-    [`(car ,e0)            `(car ,(label-lambda e0))]
-    [`(cdr ,e0)            `(cdr ,(label-lambda e0))]
-    [`(add1 ,e0)           `(add1 ,(label-lambda e0))]
-    [`(sub1 ,e0)           `(sub1 ,(label-lambda e0))]
-    [`(zero? ,e0)          `(zero? ,(label-lambda e0))]
-    [`(empty? ,e0)         `(empty? ,(label-lambda e0))]
-    [`(if ,e0 ,e1 ,e2)     `(if ,(label-lambda e0) (label-lambda e1) (label-lambda e2))]
-    [`(+ ,e0 ,e1)          `(+ ,(label-lambda e0) ,(label-lambda e1))]
-    [`(let ((,x ,e0)) ,e1) `(let ((,x ,(label-lambda e0))) ,(label-lambda e))]
-    [`(λ ,xs ,e0)          `(λ ,xs ',(gensym) ,e0)]
-    [`(,e . ,es)           `(,(label-lambda e) ,@(map label-lambda es))]))
-
-;; LExpr -> (Listof LExpr)
-;; Extract all the lambda expressions
-(define (lambdas e)
-  (match e
-    [(? symbol? x)         '()]
-    [(? imm? i)            '()]
-    [`(box ,e0)            (lambdas e0)]
-    [`(unbox ,e0)          (lambdas e0)]
-    [`(cons ,e0 ,e1)       (append (lambdas e0) (lambdas e1))]
-    [`(car ,e0)            (lambdas e0)]
-    [`(cdr ,e0)            (lambdas e0)]
-    [`(add1 ,e0)           (lambdas e0)]
-    [`(sub1 ,e0)           (lambdas e0)]
-    [`(zero? ,e0)          (lambdas e0)]
-    [`(empty? ,e0)         (lambdas e0)]
-    [`(if ,e0 ,e1 ,e2)     (append (lambdas e0) (lambdas e1) (lambdas e2))]
-    [`(+ ,e0 ,e1)          (append (lambdas e0) (lambdas e1))]
-    [`(let ((,x ,e0)) ,e1) (append (lambdas e0) (lambdas e1))]
-    [`(λ ,xs ,l ,e0)       (cons e (lambdas e0))]
-    [`(,e . ,es)           (append (lambdas e) (apply append (map lambdas es)))]))
-
-;; LExpr -> (Listof Variable)
-(define (fvs/unique e)  
-  (remove-duplicates (fvs e)))
-
-;; LExpr -> (Listof Variable)
-(define (fvs e)
-  (match e
-    [(? symbol? x)         (list x)]
-    [(? imm? i)            '()]
-    [`(box ,e0)            (fvs e0)]
-    [`(unbox ,e0)          (fvs e0)]
-    [`(cons ,e0 ,e1)       (append (fvs e0) (fvs e1))]
-    [`(car ,e0)            (fvs e0)]
-    [`(cdr ,e0)            (fvs e0)]
-    [`(add1 ,e0)           (fvs e0)]
-    [`(sub1 ,e0)           (fvs e0)]
-    [`(zero? ,e0)          (fvs e0)]
-    [`(empty? ,e0)         (fvs e0)]
-    [`(if ,e0 ,e1 ,e2)     (append (fvs e0) (fvs e1) (fvs e2))]
-    [`(+ ,e0 ,e1)          (append (fvs e0) (fvs e1))]
-    [`(let ((,x ,e0)) ,e1) (append (fvs e0) (remq* (list x) (fvs e1)))]
-    [`(λ ,xs ,l ,e0)       (remq* xs (fvs e0))]
-    [`(,e . ,es)           (append (fvs e) (apply append (map fvs es)))]))
-  
-
+    [`(λ ,xs ',f ,e0)
+     (let ((c0 (compile-tail-e e0 (append (reverse (fvs/unique l)) (reverse xs) ))))
+       `(,(symbol->label f)
+         ,@c0
+         ret))]))
 
 ;; LExpr CEnv -> Asm
 ;; Compile an expression in tail position
@@ -169,23 +102,35 @@
     [`(λ ,xs ',l ,e0)      (compile-lambda xs l e0 c)]
     [`(,e . ,es)           (compile-call e es c)]))
 
-(define (compile-lambda xs f e0 c)  
-  `(; rax <- address of label f
-    (lea rax (offset ,(symbol->label f) 0))
-    ; write in to heap
-    (mov (offset rdi 0) rax)
+(define (compile-lambda xs f e0 c)
+  (let ((fvs (fvs/unique `(λ ,xs ',f ,e0))))
+    `(; rax <- address of label f
+      (lea rax (offset ,(symbol->label f) 0))
+      ; write in to heap
+      (mov (offset rdi 0) rax)
+      
+      ;; Save the environment
+      (mov rax ,(length fvs))
+      (mov (offset rdi 1) rax) ; save length
+      ,@(copy-env-to-heap fvs c 2)
+      
+      ; rax <- pointer into heap
+      (mov rax rdi)
+      ; tag as procedure pointer
+      (or rax ,type-proc)
+      ; alloc
+      (add rdi ,(* 8 (+ 2 (length fvs)))))))
 
-    ;; Save the environment
-    ;; ...
-    
-    ; rax <- pointer into heap
-    (mov rax rdi)
-    ; tag as procedure pointer
-    (or rax ,type-proc)
-    ; alloc
-    (add rdi 8)))
+;; (Listof Variable) CEnv Natural -> Asm
+(define (copy-env-to-heap fvs c i)
+  (match fvs
+    ['() '()]
+    [(cons x fvs)
+     `((mov rax (offset rsp ,(- (add1 (lookup x c)))))
+       (mov (offset rdi ,i) rax)
+       ,@(copy-env-to-heap fvs c (add1 i)))]))
 
-;; Integer Integer -> Asm
+;; Natural Natural -> Asm
 ;; Move i arguments upward on stack by offset off
 (define (move-args i off)
   (match i
@@ -193,19 +138,6 @@
         [_ `(,@(move-args (sub1 i) off)
              (mov rbx (offset rsp ,(- off i)))
              (mov (offset rsp ,(- i)) rbx))]))
-
-;; Variable -> Asm
-(define (compile-fun f)
-  `(; rax <- address of label f
-    (lea rax (offset ,(symbol->label f) 0))
-    ; write in to heap
-    (mov (offset rdi 0) rax)
-    ; rax <- pointer into heap
-    (mov rax rdi)
-    ; tag as procedure pointer
-    (or rax ,type-proc)
-    ; alloc
-    (add rdi 8)))
 
 ;; LExpr (Listof LExpr) CEnv -> Asm
 (define (compile-call e0 es c)
@@ -218,14 +150,10 @@
       ,@cs
       (mov rax (offset rsp ,i))
       ,@assert-proc
-
-      ;; Copy saved environment to stack
-      ;; Adjust stack size appropriately
-      ;; ...
-      
-      (sub rsp ,stack-size)
       (xor rax ,type-proc)
-      (call (offset rax 0))
+      (sub rsp ,stack-size)      
+      ,@(copy-closure-env-to-stack (add1 (length es)))
+      (call (offset rax 0))      
       (add rsp ,stack-size))))
 
 ;; LExpr (Listof LExpr) CEnv -> Asm
@@ -240,7 +168,29 @@
       ,@(move-args (length es) i)
       ,@assert-proc
       (xor rax ,type-proc)
+      ,@(copy-closure-env-to-stack (length es))
       (jmp (offset rax 0)))))
+
+;; Natural -> Asm
+;; Copy closure's (in rax) env to stack skipping n spots
+(define (copy-closure-env-to-stack n)
+  (let ((copy-loop (gensym 'copy_closure))
+        (copy-done (gensym 'copy_done)))
+    `((mov r8 (offset rax 1)) ; length
+      (mov r9 rax)
+      (add r9 16)             ; start of env
+      (mov rcx rsp)           ; start of stack
+      (add rcx ,(- (* 8 (add1 n))))
+      ,copy-loop
+      (cmp r8 0)
+      (je ,copy-done)                  
+      (mov rbx (offset r9 0))       
+      (mov (offset rcx 0) rbx)
+      (sub r8 1)
+      (add r9 8)
+      (sub rcx 8)      
+      (jmp ,copy-loop)
+      ,copy-done)))
 
 ;; (Listof LExpr) CEnv -> Asm
 (define (compile-es es c)
@@ -252,24 +202,6 @@
        `(,@c0
          (mov (offset rsp ,(- (add1 (length c)))) rax)
          ,@cs))]))
-
-;; Variable (Listof Variable) LExpr -> Asm
-(define (compile-define f xs e0)
-  (let ((c0 (compile-tail-e e0 (reverse xs))))
-    `(,(symbol->label f)
-      ,@c0
-      ret)))
-
-;; (Listof Variable) (Listof (Listof Variable)) (Listof LExpr) -> Asm
-(define (compile-defines fs xss es)
-  (append-map compile-define fs xss es))
-
-;; Any -> Boolean
-(define (imm? x)
-  (or (integer? x)
-      (boolean? x)
-      (char? x)
-      (equal? ''() x)))
 
 ;; Imm -> Asm
 (define (compile-imm i)
