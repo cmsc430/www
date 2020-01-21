4e7227797e79c9bc17ffd648e3d479be9e888eca
David Van Horn <dvanhorn@cs.umd.edu>
2020-01-21 14:56:18 -0500

    Update definitions.

--- www/defns.rkt
+++ www/defns.rkt
@@ -2,15 +2,15 @@
 (provide (all-defined-out))
 (require scribble/core scribble/html-properties scribble/manual) 
 
-(define semester "fall")
-(define year "2019")
+(define semester "spring")
+(define year "2020")
 (define courseno "CMSC 430")
 
 (define IRB "IRB") 
 (define AVW "AVW")
 
 
-(define m1-date "Oct 8")
-(define m2-date "Nov 14")
-(define final-date "Dec 14, 10:30AM")
-(define elms-url "https://myelms.umd.edu/courses/1267289")
+(define m1-date "TBD")
+(define m2-date "TBD")
+(define final-date "TBD")
+(define elms-url "TBD")
