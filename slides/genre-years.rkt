43d9edaf949bcf2463e9cbb65392cc5d5d2ec691
José Manuel Calderón Trilla <jmct@galois.com>
2020-02-01 22:51:31 -0500

    improve slides a bit

---
+++ slides/genre-years.rkt
@@ -0,0 +1,11 @@
+#lang racket
+(define best-years
+  (list (cons "classical" 1824)
+        (cons "hip hop" 2015)
+        (cons "jazz" 1959)
+        (cons "prog" 2005)
+        (cons "rock" 1967)
+        (cons "romantic" 1824)
+        (cons "soul" 1970)
+        (cons "trash" 1990)))
+(provide best-years)
