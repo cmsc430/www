cc4c424d2058fbab3501aceb1b63dd614298688c
José Manuel Calderón Trilla <jmct@galois.com>
2020-02-04 14:05:57 -0500

    correct genre-years.rkt

--- www/slides/genre-years.rkt
+++ www/slides/genre-years.rkt
@@ -1,42 +1,12 @@
 #lang racket
-(struct leaf ())
-(struct node (i left right))
-
-(define (get-val n)
-  (match n
-    [(node i _ _) i]))
-
-; Make a node with no children
-(define (make-node i)
-  (node i leaf leaf))
-
-(define (take-two ns)
-  (match ns
-    ['()                   '()]
-    [(cons a (cons b nss)) (cons
-                             (node (+ (get-val a) (get-val b)) a b)
-                             nss)]))
-
-(define (make-inner-layer new-nodes old-nodes)
-  (if (empty? old-nodes)
-    new-nodes
-    (let* ([taken (take-two old-nodes)]
-           [ns (append new-nodes (list (car taken)))]
-           [os (cdr taken)])
-      (make-inner-layer ns os))))
-
-(define (make-tree depth)
-  (if (<= depth 0)
-    leaf
-    (letrec
-      ; The values that will exist at the bottom of the tree
-      ([final-nodes (map make-node (range 1 (+ 1 (expt 2 depth))))]
-       [go (λ (prev-layer d)
-              (if (<= d 1)
-                prev-layer
-                (let ([new-layer (make-inner-layer '() prev-layer)])
-                  (go new-layer (- d 1)))))])
-      (car (go final-nodes depth)))))
-
+(define best-years
+  (list
+    (cons "classical" 1824)
+    (cons "hip hop"   2015)
+    (cons "jazz"      1959)
+    (cons "prog"      2003)
+    (cons "rock"      1967)
+    (cons "romantic"  1824)
+    (cons "thrash"    1990)))
 
 (provide (all-defined-out))
