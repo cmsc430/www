579549096a49151ed0f07222bf251613bc972367
José Manuel Calderón Trilla <jmct@galois.com>
2020-02-17 22:28:23 -0500

    Allow Fraud to accept boolean expressions
    I believe that this was just an oversight, because as far as I can tell,
    the compiler and interpreter supported boolean expressions _already_. It
    was just that the syntax validation seems to have been taken from `con`
    and not `dupe` or `extort`.

--- www/notes/fraud/syntax.rkt
+++ www/notes/fraud/syntax.rkt
@@ -5,11 +5,13 @@
 ;; Is x a well-formed expression?
 (define (expr? x)
   (match x
-    [(? symbol?) (not (memq x '(if let add1 sub1)))]
-    [(? integer? i) #t]
+    [(? symbol?) (not (memq x '(if let add1 sub1 zero?)))]
+    [(? integer?) #t]
+    [(? boolean?) #t]
     [`(add1 ,x) (expr? x)]
     [`(sub1 ,x) (expr? x)]
-    [`(if (zero? ,x) ,y ,z)
+    [`(zero? ,x) (expr? x)]
+    [`(if ,x ,y ,z)
      (and (expr? x)
           (expr? y)
           (expr? z))]    
