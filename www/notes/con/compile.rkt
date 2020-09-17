37767dd78d8b16c18a13725e822d24ea861b4839
José Manuel Calderón Trilla <jmct@umd.edu>
2020-09-17 14:17:51 -0400

    Update con

--- www/notes/con/compile.rkt
+++ www/notes/con/compile.rkt
@@ -1,5 +1,6 @@
 #lang racket
 (provide (all-defined-out))
+(require "ast.rkt")
 
 ;; Expr -> Asm
 (define (compile e)
@@ -10,26 +11,23 @@
 ;; Expr -> Asm
 (define (compile-e e)
   (match e
-    [(? integer? i) `((mov rax ,i))]
-    [`(add1 ,e0)
-     (let ((c0 (compile-e e0)))
-       `(,@c0
-         (add rax 1)))]    
-    [`(sub1 ,e0)
-     (let ((c0 (compile-e e0)))
-       `(,@c0
-         (sub rax 1)))]
-    [`(if (zero? ,e0) ,e1 ,e2)
-     (let ((c0 (compile-e e0))
-           (c1 (compile-e e1))
-           (c2 (compile-e e2))
-           (l0 (gensym "if"))
-           (l1 (gensym "if")))
-       `(,@c0
-         (cmp rax 0)
-         (jne ,l0)
-         ,@c1
-         (jmp ,l1)
-         ,l0
-         ,@c2
-         ,l1))]))
+    [(int-e i) `((mov rax ,i))]
+    [(add1-e e1) (let ((c1 (compile-e e1)))
+                `(,@c1
+                  (add rax 1)))]
+    [(sub1-e e1) (let ((c1 (compile-e e1)))
+                `(,@c1
+                  (sub rax 1)))]
+    [(if-e i t f) (let ((c1 (compile-e i))
+                        (c2 (compile-e t))
+                        (c3 (compile-e f))
+                        (l1 (gensym "if"))
+                        (l2 (gensym "if")))
+                    `(,@c1
+                      (cmp rax 0) ; zero? <result of executing code for i>
+                      (jne ,l1)
+                      ,@c2
+                      (jmp ,l2)
+                      ,l1
+                      ,@c3
+                      ,l2))]))
