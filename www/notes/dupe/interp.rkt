191453c1a8ee3062a5b08df91233e58bb692c572
José Manuel Calderón Trilla <jmct@umd.edu>
2020-09-20 16:58:44 -0400

    Use new AST structures for DUPE
    Tests still need to be updated

--- www/notes/dupe/interp.rkt
+++ www/notes/dupe/interp.rkt
@@ -1,6 +1,8 @@
 #lang racket
 (provide (all-defined-out))
 
+(require "ast.rkt")
+
 ;; type Value =
 ;; | Integer
 ;; | Boolean
@@ -8,15 +10,15 @@
 ;; Expr -> Value
 (define (interp e)
   (match e
-    [(? integer? i) i]
-    [(? boolean? b) b]
-    [`(add1 ,e0)
-     (add1 (interp e0))]
-    [`(sub1 ,e0)
-     (sub1 (interp e0))]
-    [`(zero? ,e0)
+    [(int-e i) i]
+    [(add1-e e0)
+     (+ (interp e0) 1)]
+    [(sub1-e e0)
+     (- (interp e0) 1)]
+    [(bool-e b) b]
+    [(zero?-e e0)
      (zero? (interp e0))]
-    [`(if ,e0 ,e1 ,e2)
+    [(if-e e0 e1 e2)
      (if (interp e0)
          (interp e1)
          (interp e2))]))
--- www/notes/dupe/syntax.rkt
+++ www/notes/dupe/syntax.rkt
@@ -1,5 +1,6 @@
 #lang racket
 (provide (all-defined-out))
+(require "ast.rkt")
 
 ;; Any -> Boolean
 ;; Is x a well-formed expression?
@@ -15,3 +16,17 @@
           (expr? y)
           (expr? z))]
     [_ #f]))
+
+
+; SExpr -> AST
+; Parse the s-expr into our AST
+; This should be a one-to-one mapping for now.
+(define (sexpr->ast s)
+  (match s
+    [(? integer? s) (int-e s)]
+    [(? boolean? b) (bool-e b)]
+    [`(add1 ,e)     (add1-e (sexpr->ast e))]
+    [`(sub1 ,e)     (sub1-e (sexpr->ast e))]
+    [`(zero? ,e)    (zero?-e (sexpr->ast e))]
+    [`(if ,p ,t ,f) (if-e (sexpr->ast p) (sexpr->ast t) (sexpr->ast f))]
+    [_              (error "operation not supported")]))
