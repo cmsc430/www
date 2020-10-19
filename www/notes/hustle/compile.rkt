07a23249f36d73323a9eda6fd70705768f856048
José Manuel Calderón Trilla <jmct@umd.edu>
2020-10-19 02:15:28 -0400

    update compiler

--- www/notes/hustle/compile.rkt
+++ www/notes/hustle/compile.rkt
@@ -1,6 +1,8 @@
 #lang racket
 (provide (all-defined-out))
 
+(require "ast.rkt")
+
 ;; An immediate is anything ending in #b000
 ;; All other tags in mask #b111 are pointers
 
@@ -36,35 +38,41 @@
 ;; Expr CEnv -> Asm
 (define (compile-e e c)
   (match e
-    [(? imm? i)            (compile-imm i)]
-    [(? symbol? x)         (compile-var x c)]    
-    [`(box ,e0)            (compile-box e0 c)]
-    [`(unbox ,e0)          (compile-unbox e0 c)]
-    [`(cons ,e0 ,e1)       (compile-cons e0 e1 c)]
-    [`(car ,e0)            (compile-car e0 c)]
-    [`(cdr ,e0)            (compile-cdr e0 c)]
-    [`(add1 ,e0)           (compile-add1 e0 c)]
-    [`(sub1 ,e0)           (compile-sub1 e0 c)]
-    [`(zero? ,e0)          (compile-zero? e0 c)]
-    [`(map-zero? ,e0)      (compile-map-zero? e0 c)]
-    [`(if ,e0 ,e1 ,e2)     (compile-if e0 e1 e2 c)]
-    [`(let ((,x ,e0)) ,e1) (compile-let x e0 e1 c)]
-    [`(+ ,e0 ,e1)          (compile-+ e0 e1 c)]
-    [`(- ,e0 ,e1)          (compile-- e0 e1 c)]))
+    [(? imm? i)              (compile-imm i)]
+    [(var-e v)               (compile-var v c)]
+    [(prim-e (? prim? p) es) (compile-prim p es c)]
+    [(if-e p t f)            (compile-if p t f c)]
+    [(let-e (list b) body)   (compile-let b body c)]))
 
 ;; Any -> Boolean
 (define (imm? x)
-  (or (integer? x)
-      (boolean? x)
-      (equal? ''() x)))
+  (or (int-e? x)
+      (bool-e? x)
+      (nil-e? x)))
 
 ;; Imm -> Asm
 (define (compile-imm i)
   `((mov rax
          ,(match i
-            [(? integer? i) (arithmetic-shift i imm-shift)]
-            [(? boolean? b) (if b imm-val-true imm-val-false)]
-            [''()           imm-val-empty]))))
+            [(int-e i)  (arithmetic-shift i imm-shift)]
+            [(bool-e b) (if b imm-val-true imm-val-false)]
+            [(nil-e)    imm-val-empty]))))
+
+(define (compile-prim p es c)
+  (match (cons p es)
+    [`(add1 ,e0)           (compile-add1 e0 c)]
+    [`(sub1 ,e0)           (compile-sub1 e0 c)]
+    [`(zero? ,e0)          (compile-zero? e0 c)]
+    [`(+ ,e0 ,e1)          (compile-+ e0 e1 c)]
+    [`(- ,e0 ,e1)          (compile-- e0 e1 c)]
+    ;; New for Hustle
+    [`(box ,e0)            (compile-box e0 c)]
+    [`(unbox ,e0)          (compile-unbox e0 c)]
+    [`(cons ,e0 ,e1)       (compile-cons e0 e1 c)]
+    [`(car ,e0)            (compile-car e0 c)]
+    [`(cdr ,e0)            (compile-cdr e0 c)]
+    [_            (error
+                    (format "prim applied to wrong number of args: ~a ~a" p es))]))
 
 ;; Variable CEnv -> Asm
 (define (compile-var x c)
@@ -110,62 +118,6 @@
       (xor rax ,type-pair) ; untag
       (mov rax (offset rax 1)))))
 
-(define (compile-map-zero? e0 c)
-  (let ((c0 (compile-e e0 c))
-        (lend (gensym "end"))
-        (lz1 (gensym "nz"))
-        (lz2 (gensym "z"))
-        (lempty (gensym "empty"))
-        (loop (gensym "loop_start")))
-   `(,@c0
-
-     (cmp rax ,imm-val-empty) ; if the input list is empty, we're done
-     (je ,lend)
-     ,@assert-pair            ; otherwise, it should be a pair (cons)
-
-     (mov (offset rsp ,(- (add1 (length c)))) rdi) ; store initial heap pointer
-
-     ,loop                    ; Start of our loop
-
-     (xor rax ,type-pair)     ; untag the pair
-
-     (mov rcx rax)            ; store pointer in rcx
-
-     (mov rax (offset rax 1)) ; check that `car` is an integer
-     ,@assert-integer
-     ,@(zero?-asm lz1 lz2)       ; rax is zero?, leave the bool in rax
-
-     (mov (offset rdi 1) rax) ; put the boolean in the car of the new list cell
-
-     (mov rax (offset rcx 0)) ; move cdr of input into rax 
-     
-     (cmp rax ,imm-val-empty) ; if the cdr is the empty list, finish up
-     (je ,lempty)
-
-     ,@assert-pair            ; if it's not empty, it better be a pair
-
-     (mov rdx rdi)            ; move a copy of the heap pointer into rdx
-     (add rdx 16)             ; figure out where the next cons is going to be
-     (or rdx ,type-pair)      ; tag the next cons as a pair
-     (mov (offset rdi 0) rdx) ; the cdr of our new cons is where the next one goes
-
-     (add rdi 16)             ; bump the heap pointer
-
-                              ; since rax wasn't the empty list
-                              ; it's a pointer to out next cons
-     (jmp ,loop)              ; iterate 
-
-     ,lempty
-     (mov rax ,imm-val-empty)
-     (mov (offset rdi 0) rax)
-     (mov rax rdi)
-     (add rdi 16)
-
-     (mov rax (offset rsp ,(- (add1 (length c))))) ; Get initial heap pointer back
-     (or rax ,type-pair)      ; tag the initial thing as a pair
-     ,lend
-     
-    )))
 
 (define (zero?-asm l0 l1)
     `(,@assert-integer
@@ -223,12 +175,15 @@
       ,l1)))
 
 ;; Variable Expr Expr CEnv -> Asm
-(define (compile-let x e0 e1 c)
-  (let ((c0 (compile-e e0 c))
-        (c1 (compile-e e1 (cons x c))))
-    `(,@c0
-      (mov (offset rsp ,(- (add1 (length c)))) rax)
-      ,@c1)))
+(define (compile-let b e1 c)
+  (match b
+    [(binding v def)
+       (let ((c0 (compile-e def c))
+             (c1 (compile-e e1 (cons v c))))
+         `(,@c0
+           (mov (offset rsp ,(- (add1 (length c)))) rax)
+           ,@c1))]
+    [_ (error "Compile-let can only handle bindings")]))
 
 ;; Expr Expr CEnv -> Asm
 (define (compile-+ e0 e1 c) 
@@ -270,3 +225,62 @@
 (define assert-integer (assert-type imm-type-mask imm-type-int))
 (define assert-box     (assert-type result-type-mask type-box))
 (define assert-pair    (assert-type result-type-mask type-pair))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;; You don't need to know this, it's bonus fun
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (compile-map-zero? e0 c)
+  (let ((c0 (compile-e e0 c))
+        (lend (gensym "end"))
+        (lz1 (gensym "nz"))
+        (lz2 (gensym "z"))
+        (lempty (gensym "empty"))
+        (loop (gensym "loop_start")))
+   `(,@c0
+
+     (cmp rax ,imm-val-empty) ; if the input list is empty, we're done
+     (je ,lend)
+     ,@assert-pair            ; otherwise, it should be a pair (cons)
+
+     (mov (offset rsp ,(- (add1 (length c)))) rdi) ; store initial heap pointer
+
+     ,loop                    ; Start of our loop
+
+     (xor rax ,type-pair)     ; untag the pair
+
+     (mov rcx rax)            ; store pointer in rcx
+
+     (mov rax (offset rax 1)) ; check that `car` is an integer
+     ,@assert-integer
+     ,@(zero?-asm lz1 lz2)       ; rax is zero?, leave the bool in rax
+
+     (mov (offset rdi 1) rax) ; put the boolean in the car of the new list cell
+
+     (mov rax (offset rcx 0)) ; move cdr of input into rax
+
+     (cmp rax ,imm-val-empty) ; if the cdr is the empty list, finish up
+     (je ,lempty)
+
+     ,@assert-pair            ; if it's not empty, it better be a pair
+
+     (mov rdx rdi)            ; move a copy of the heap pointer into rdx
+     (add rdx 16)             ; figure out where the next cons is going to be
+     (or rdx ,type-pair)      ; tag the next cons as a pair
+     (mov (offset rdi 0) rdx) ; the cdr of our new cons is where the next one goes
+
+     (add rdi 16)             ; bump the heap pointer
+
+                              ; since rax wasn't the empty list
+                              ; it's a pointer to out next cons
+     (jmp ,loop)              ; iterate
+
+     ,lempty
+     (mov rax ,imm-val-empty)
+     (mov (offset rdi 0) rax)
+     (mov rax rdi)
+     (add rdi 16)
+
+     (mov rax (offset rsp ,(- (add1 (length c))))) ; Get initial heap pointer back
+     (or rax ,type-pair)      ; tag the initial thing as a pair
+     ,lend)))
