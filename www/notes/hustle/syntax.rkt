9c32cd1c0b9c1528a14d360c1a313a188acd3fff
José Manuel Calderón Trilla <jmct@umd.edu>
2020-10-19 01:09:13 -0400

    Update AST and syntax

--- www/notes/hustle/syntax.rkt
+++ www/notes/hustle/syntax.rkt
@@ -1,15 +1,7 @@
 #lang racket
 (provide (all-defined-out))
 
-;; Any -> Boolean
-(define (binop? x)
-  (and (symbol? x)
-       (memq x '(+ - cons))))
-
-;; Any -> Boolean
-(define (unop? x)
-  (and (symbol? x)
-       (memq x '(add1 sub1 zero? box unbox car cdr map-zero?))))
+(require "ast.rkt")
 
 ;; Any -> Boolean
 ;; Is x a well-formed expression?
@@ -20,7 +12,7 @@
     [(? boolean?) #t]
     [''() #t]
     [`(,(? unop? p) ,x) (expr? x)]
-    [`(,(? binop? p) ,x ,y) 
+    [`(,(? biop? p) ,x ,y) 
       (and
         (expr? x)
         (expr? y))]
@@ -33,3 +25,22 @@
           (expr? y)
           (expr? z))]
     [_ #f]))
+
+; SExpr -> AST
+; Parse the s-expr into our AST
+; This should be a one-to-one mapping for now.
+(define (sexpr->ast s)
+  (match s
+    [(? symbol? v)  (var-e v)]
+    [(? integer? s) (int-e s)]
+    [(? boolean? b) (bool-e b)]
+    [''()           (nil-e)]
+    [`(if ,p ,t ,f) (if-e (sexpr->ast p) (sexpr->ast t) (sexpr->ast f))]
+    [`(let ((,bnd ,def)) ,body) (let-e (list (binding bnd (sexpr->ast def))) (sexpr->ast body))]
+    [`(,p ,e)       (if (unop? p)
+                        (prim-e p (list (sexpr->ast e)))
+                        (error (format "~a is not a primitive" p)))]
+    [`(,p ,e1, e2)  (if (biop? p)
+                        (prim-e p (list (sexpr->ast e1) (sexpr->ast e2)))
+                        (error (format "~a is not a primitive" p)))]
+    [_              (error "operation not supported")]))
