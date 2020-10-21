f9c5ac044c4d00b4a79c78b3f74392f4e80ccc4d
José Manuel Calderón Trilla <jmct@umd.edu>
2020-10-21 19:25:26 -0400

    Update iniquity AST

--- www/notes/iniquity/syntax.rkt
+++ www/notes/iniquity/syntax.rkt
@@ -1,6 +1,8 @@
 #lang racket
 (provide (all-defined-out))
 
+(require "ast.rkt")
+
 ;; Any -> Boolean
 (define (binop? x)
   (and (symbol? x)
@@ -43,3 +45,33 @@
       (foldr (lambda (x y) (and x y)) #t (map expr? es))]
 
     [_ #f]))
+
+; SExpr -> Prog
+(define (sexpr->prog s)
+  (match s
+    [(list 'begin defs ... e) (prog (map sexpr->fundef defs) (sexpr->expr e))]
+    [e                        (prog '() (sexpr->expr e))]))
+
+; SExpr -> FunDef
+(define (sexpr->fundef def)
+  (match def
+    [`(define (,f . ,as) ,body) (fundef f as body)]))
+
+; SExpr -> Expr
+; Parse the s-expr into our Expr AST
+; This should be a one-to-one mapping for now.
+(define (sexpr->expr s)
+  (match s
+    [(? symbol? v)  (var-e v)]
+    [(? integer? s) (int-e s)]
+    [(? boolean? b) (bool-e b)]
+    [''()           (nil-e)]
+    [`(if ,p ,t ,f) (if-e (sexpr->expr p) (sexpr->expr t) (sexpr->expr f))]
+    [`(let ((,bnd ,def)) ,body) (let-e (list (binding bnd (sexpr->expr def))) (sexpr->expr body))]
+    [`(,p ,e)       (if (unop? p)
+                        (prim-e p (list (sexpr->expr e)))
+                        (error (format "~a is not a primitive" p)))]
+    [`(,p ,e1 ,e2)  (if (biop? p)
+                        (prim-e p (list (sexpr->expr e1) (sexpr->expr e2)))
+                        (error (format "~a is not a primitive" p)))]
+    [_              (error "operation not supported")]))
