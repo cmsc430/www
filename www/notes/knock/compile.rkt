36a1b23f8f9188e95a9d4a5f625fcb92a10d2c25
José Manuel Calderón Trilla <jmct@umd.edu>
2020-10-26 00:37:23 -0400

    New AST for knock

--- www/notes/knock/compile.rkt
+++ www/notes/knock/compile.rkt
@@ -1,6 +1,8 @@
 #lang racket
 (provide (all-defined-out))
 
+(require "ast.rkt")
+
 ;; An immediate is anything ending in #b000
 ;; All other tags in mask #b111 are pointers
 
@@ -26,22 +28,15 @@
 ;; end in #b000 and we tag with #b001 for boxes, etc.
 
 ;; type CEnv = (Listof (Maybe Variable))
-;; type Imm = Integer | Boolean | Char | ''()
-
-;; type Prog =
-;; | Expr
-;; | `(begin ,@(Listof (define (,Variable ,@(Listof Variable)) ,Expr))
-;;           ,Expr)
 
 ;; Prog -> Asm
 (define (compile p)
   (match p
-    [(list 'begin `(define (,fs . ,xss) ,es) ... e0)
-     (let ((ds (compile-defines fs xss es))
-           (c0 (compile-entry e0)))
+    [(prog defs e)
+     (let ((ds (compile-defines defs))
+           (c0 (compile-entry e)))
        `(,@c0
-         ,@ds))]
-    [e (compile-entry e)]))
+         ,@ds))]))
 
 ;; Expr -> Asm
 ;; Compile e as the entry point
@@ -49,7 +44,6 @@
   `(entry
     ,@(compile-tail-e e '())
     ret
-
     err
     (push rbp)
     (call error)
@@ -59,30 +53,32 @@
 ;; Compile an expression in tail position
 (define (compile-tail-e e c)
   (match e
-    [(? symbol? x)         (compile-variable x c)]
-    [(? imm? i)            (compile-imm i)]
-    [`(box ,e0)            (compile-box e0 c)]
-    [`(unbox ,e0)          (compile-unbox e0 c)]
-    [`(cons ,e0 ,e1)       (compile-cons e0 e1 c)]
-    [`(car ,e0)            (compile-car e0 c)]
-    [`(cdr ,e0)            (compile-cdr e0 c)]
-    [`(add1 ,e0)           (compile-add1 e0 c)]
-    [`(sub1 ,e0)           (compile-sub1 e0 c)]
-    [`(zero? ,e0)          (compile-zero? e0 c)]
-    [`(empty? ,e0)         (compile-empty? e0 c)]
-    [`(if ,e0 ,e1 ,e2)     (compile-tail-if e0 e1 e2 c)]
-    [`(+ ,e0 ,e1)          (compile-+ e0 e1 c)]
-    [`(let ((,x ,e0)) ,e1) (compile-tail-let x e0 e1 c)]
-    [`(fun ,f)             (compile-fun f)]
-    [`(call ,e0 . ,es)     (compile-fun-tail-call e0 es c)]
-    [`(,f . ,es)           (compile-tail-call f es c)]))
+    [(var-e v)               (compile-variable v c)]
+    [(? imm? i)              (compile-imm i)]
+    [(prim-e (? prim? p) es) (compile-prim p es c)]
+    [(if-e p t f)            (compile-tail-if p t f c)]
+    [(let-e (list b) body)   (compile-tail-let b body c)]
+    [(fun-e f)               (compile-fun f)]
+    [(call-e f es)           (compile-fun-tail-call f es c)]
+    [(app-e f es)            (compile-tail-call f es c)]))
 
 ;; Expr CEnv -> Asm
 ;; Compile an expression in non-tail position
 (define (compile-e e c)
   (match e
-    [(? symbol? x)         (compile-variable x c)]
-    [(? imm? i)            (compile-imm i)]
+    [(var-e v)               (compile-variable v c)]
+    [(? imm? i)              (compile-imm i)]
+    [(prim-e (? prim? p) es) (compile-prim p es c)]
+    [(if-e p t f)            (compile-if p t f c)]
+    [(let-e (list b) body)   (compile-let b body c)]
+    [(fun-e f)               (compile-fun f)]
+    [(call-e f es)           (compile-fun-call f es c)]
+    [(app-e f es)            (compile-call f es c)]))
+
+;; Our current set of primitive operations require no function calls,
+;; so there's no difference between tail and non-tail call positions
+(define (compile-prim p es c)
+  (match (cons p es)
     [`(box ,e0)            (compile-box e0 c)]
     [`(unbox ,e0)          (compile-unbox e0 c)]
     [`(cons ,e0 ,e1)       (compile-cons e0 e1 c)]
@@ -92,12 +88,9 @@
     [`(sub1 ,e0)           (compile-sub1 e0 c)]
     [`(zero? ,e0)          (compile-zero? e0 c)]
     [`(empty? ,e0)         (compile-empty? e0 c)]
-    [`(if ,e0 ,e1 ,e2)     (compile-if e0 e1 e2 c)]
     [`(+ ,e0 ,e1)          (compile-+ e0 e1 c)]
-    [`(let ((,x ,e0)) ,e1) (compile-let x e0 e1 c)]
-    [`(fun ,f)             (compile-fun f)]
-    [`(call ,e0 . ,es)     (compile-fun-call e0 es c)]
-    [`(,f . ,es)           (compile-call f es c)]))
+    [_            (error
+                    (format "prim applied to wrong number of args: ~a ~a" p es))]))
 
 ;; Variable (Listof Expr) CEnv -> Asm
 ;; Statically know the function we're calling
@@ -182,22 +175,24 @@
          ,@cs))]))
 
 ;; Variable (Listof Variable) Expr -> Asm
-(define (compile-define f xs e0)
-  (let ((c0 (compile-tail-e e0 (reverse xs))))
-    `(,(symbol->label f)
-      ,@c0
-      ret)))
+(define (compile-define def)
+  (match def
+    [(fundef name args body)
+      (let ((c0 (compile-e body (reverse args))))
+        `(,(symbol->label name)
+          ,@c0
+          ret))]))
 
 ;; (Listof Variable) (Listof (Listof Variable)) (Listof Expr) -> Asm
-(define (compile-defines fs xss es)
-  (append-map compile-define fs xss es))
+(define (compile-defines defs)
+  (append-map compile-define defs))
 
 ;; Any -> Boolean
 (define (imm? x)
-  (or (integer? x)
-      (boolean? x)
-      (char? x)
-      (equal? ''() x)))
+  (or (int-e? x)
+      (bool-e? x)
+      (char-e? x)
+      (nil-e? x)))
 
 ;; Imm -> Asm
 (define (compile-imm i)
@@ -206,10 +201,10 @@
 ;; Imm -> Integer
 (define (imm->bits i)
   (match i
-    [(? integer? i) (arithmetic-shift i imm-shift)]
-    [(? char? c)    (+ (arithmetic-shift (char->integer c) imm-shift) imm-type-char)]
-    [(? boolean? b) (if b imm-val-true imm-val-false)]
-    [''()           imm-type-empty]))
+    [(int-e i)  (arithmetic-shift i imm-shift)]
+    [(char-e c) (+ (arithmetic-shift (char->integer c) imm-shift) imm-type-char)]
+    [(bool-e b) (if b imm-val-true imm-val-false)]
+    [(nil-e)    imm-type-empty]))
 
 ;; Variable CEnv -> Asm
 (define (compile-variable x c)
@@ -335,20 +330,26 @@
       ,l1)))
 
 ;; Variable Expr Expr CEnv -> Asm
-(define (compile-tail-let x e0 e1 c)
-  (let ((c0 (compile-e e0 c))
-        (c1 (compile-tail-e e1 (cons x c))))
-    `(,@c0
-      (mov (offset rsp ,(- (add1 (length c)))) rax)
-      ,@c1)))
+(define (compile-tail-let b e1 c)
+  (match b
+    [(binding v def)
+       (let ((c0 (compile-e def c))
+             (c1 (compile-tail-e e1 (cons v c))))
+         `(,@c0
+           (mov (offset rsp ,(- (add1 (length c)))) rax)
+           ,@c1))]
+    [_ (error "Compile-let can only handle bindings")]))
 
 ;; Variable Expr Expr CEnv -> Asm
-(define (compile-let x e0 e1 c)
-  (let ((c0 (compile-e e0 c))
-        (c1 (compile-e e1 (cons x c))))
-    `(,@c0
-      (mov (offset rsp ,(- (add1 (length c)))) rax)
-      ,@c1)))
+(define (compile-let b e1 c)
+  (match b
+    [(binding v def)
+       (let ((c0 (compile-e def c))
+             (c1 (compile-e e1 (cons v c))))
+         `(,@c0
+           (mov (offset rsp ,(- (add1 (length c)))) rax)
+           ,@c1))]
+    [_ (error "Compile-let can only handle bindings")]))
 
 ;; Expr Expr CEnv -> Asm
 (define (compile-+ e0 e1 c)
