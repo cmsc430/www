9f8efeaa67aa4b7172c45e949037c2f1c63f9024
David Van Horn <dvanhorn@cs.umd.edu>
2020-11-04 18:02:32 -0500

    AST treatment for Con.

--- www/notes/con/ast.rkt
+++ www/notes/con/ast.rkt
@@ -1,22 +1,12 @@
 #lang racket
-(provide (all-defined-out))
+(provide Int Add1 Sub1 IfZero)
 
 ;; type Expr =
-;; | Integer
-;; | `(add1 ,Expr)
-;; | `(sub1 ,Expr)
-;; | `(if (zero? ,Expr) ,Expr ,Expr)
-
-(struct int-e (i))
-(struct add1-e (e))
-(struct sub1-e (e))
-(struct if-e (i t f))
-
-(define (ast->sexpr a)
-  (match a
-    [(int-e i)    `(int-e ,i)]
-    [(add1-e e)   `(add1-e ,(ast->sexpr e))]
-    [(sub1-e e)   `(sub1-e ,(ast->sexpr e))]
-    [(if-e i t f) `(if-e (zero? ,(ast->sexpr i))
-                         ,(ast->sexpr t)
-                         ,(ast->sexpr f))]))
+;; | (Int Integer)
+;; | (Add1 Expr)
+;; | (Sub1 Expr)
+;; | (IfZero Expr Expr Expr)
+(struct Int (i) #:prefab)
+(struct Add1 (e) #:prefab)
+(struct Sub1 (e) #:prefab)
+(struct IfZero (e1 e2 e3) #:prefab)
--- www/notes/con/compile.rkt
+++ www/notes/con/compile.rkt
@@ -1,33 +1,32 @@
 #lang racket
 (provide (all-defined-out))
 (require "ast.rkt")
+(require "asm/ast.rkt")
 
 ;; Expr -> Asm
 (define (compile e)
-  `(entry
-    ,@(compile-e e)
-    ret))
+  (append (list (Label 'entry))
+          (compile-e e)
+          (list (Ret))))
 
 ;; Expr -> Asm
 (define (compile-e e)
   (match e
-    [(int-e i) `((mov rax ,i))]
-    [(add1-e e1) (let ((c1 (compile-e e1)))
-                `(,@c1
-                  (add rax 1)))]
-    [(sub1-e e1) (let ((c1 (compile-e e1)))
-                `(,@c1
-                  (sub rax 1)))]
-    [(if-e i t f) (let ((c1 (compile-e i))
-                        (c2 (compile-e t))
-                        (c3 (compile-e f))
-                        (l1 (gensym "if"))
-                        (l2 (gensym "if")))
-                    `(,@c1
-                      (cmp rax 0) ; zero? <result of executing code for i>
-                      (je ,l1)
-                      ,@c3
-                      (jmp ,l2)
-                      ,l1
-                      ,@c2
-                      ,l2))]))
+    [(Int i) (list (Mov 'rax i))]
+    [(Add1 e1)
+     (append (compile-e e1)
+             (list (Add 'rax 1)))]
+    [(Sub1 e1) 
+     (append (compile-e e1)
+             (list (Sub 'rax 1)))]
+    [(IfZero e1 e2 e3)
+     (let ((l1 (gensym 'if))
+           (l2 (gensym 'if)))
+       (append (compile-e e1)
+               (list (Cmp 'rax 0)
+                     (Je l1))
+               (compile-e e3)
+               (list (Jmp l2)
+                     (Label l1))
+               (compile-e e2)
+               (list (Label l2))))]))
