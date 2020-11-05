ac46ac95b1c7cfd955d530363f5177049881846c
David Van Horn <dvanhorn@cs.umd.edu>
2020-11-05 14:56:33 -0500

    AST treatment for Extort.

--- www/notes/extort/test/compile-rand.rkt
+++ www/notes/extort/test/compile-rand.rkt
@@ -1,10 +1,9 @@
 #lang racket
-(require "../interp.rkt" "../compile.rkt" "../asm/interp.rkt" "../random.rkt" "../syntax.rkt" rackunit)
+(require "../interp.rkt" "../compile.rkt" "../asm/interp.rkt" "../random.rkt" rackunit)
 
 (define (check-compiler e)
-  (let ((e (sexpr->ast e)))
-    (check-eqv? (interp e)
-                (asm-interp (compile e)))))
+  (check-eqv? (interp e)
+              (asm-interp (compile e))))
 
 (for ([i (in-range 500)])
   (check-compiler (random-expr)))
