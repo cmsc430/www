f896a2e4dbafad9711bcb8f2452ca28fe332741b
David Van Horn <dvanhorn@cs.umd.edu>
2020-11-20 01:47:53 -0500

    Fix Blackmail interp.

--- www/notes/blackmail/interp.rkt
+++ www/notes/blackmail/interp.rkt
@@ -6,5 +6,5 @@
 (define (interp e)
   (match e
     [(Int i) i]
-    [(Add1 e) (add1 (interp e))]
-    [(Sub1 e) (sub1 (interp e))]))
+    [(Prim 'add1 e) (add1 (interp e))]
+    [(Prim 'sub1 e) (sub1 (interp e))]))
