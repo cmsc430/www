978ba24eaa6883babf0e5f76d50d1578974a926c
David Van Horn <dvanhorn@cs.umd.edu>
2020-11-23 13:34:39 -0500

    Fixing up bit layout in Iniquity.

--- www/notes/iniquity/types.h
+++ www/notes/iniquity/types.h
@@ -1,14 +1,52 @@
-#define result_shift     3
-#define result_type_mask ((1 << result_shift) - 1)
-#define type_imm         0
-#define type_box         1
-#define type_pair        2
-#define type_string      3
-#define type_proc        4
+/*
+  Bit layout of values
 
-#define imm_shift      (3 + result_shift)
-#define imm_type_mask  ((1 << imm_shift) - 1)
-#define imm_type_int   (0 << result_shift)
-#define imm_type_bool  (1 << result_shift)
-#define imm_type_char  (2 << result_shift)
-#define imm_type_empty (3 << result_shift)
+  Values are either:
+  - Immediates: end in #b000
+  - Pointers: end in anything else
+*/
+#define imm_shift 3
+#define imm_type_mask ((1 << imm_shift) - 1)
+#define imm_type_tag 0
+
+/*
+  Immediates are either:
+  - Integers: end in #b0 (& immediate tag)
+  - Non-Integers: end in #b1 (& immediate tag)
+*/
+#define int_shift        (imm_shift + 1)
+#define int_type_mask    ((1 << int_shift) - 1)
+#define int_type_tag     ((0 << (int_shift - 1)) | imm_type_tag)
+#define nonint_type_tag  ((1 << (int_shift - 1)) | imm_type_tag)
+
+/*
+  Pointers are "tagged addresses"
+  - Boxes:   end in #b001
+  - Pairs:   end in #b010
+  - remaining bit patterns are reserved for future pointers     
+  To recover the address, xor the tag to zero it out
+*/
+#define box_type_tag  1
+#define pair_type_tag 2
+
+/*
+  Non-Integers are either:
+  - Characters:     end in #b0 (& non-integer tag), code-point in remaining bits    
+  - Non-Characters: end in #b1 (& non-integer tag)
+*/
+#define char_shift       (int_shift + 1)
+#define char_type_mask   ((1 << char_shift) - 1)
+#define char_type_tag    ((0 << (char_shift - 1)) | nonint_type_tag)
+#define nonchar_type_tag ((1 << (char_shift - 1)) | nonint_type_tag)
+
+/*
+  Non-Chacters are either:
+  - True:    #b0 (& non-character tag)
+  - False:   #b1 (& non-character tag)
+  - Empty:  #b10
+  - remaining bit patterns are reserved for future values.
+*/
+#define singleton_shift (char_shift + 1)
+#define val_true  ((0 << (singleton_shift - 1)) | nonchar_type_tag)
+#define val_false ((1 << (singleton_shift - 1)) | nonchar_type_tag)
+#define val_empty ((2 << (singleton_shift - 1)) | nonchar_type_tag)
