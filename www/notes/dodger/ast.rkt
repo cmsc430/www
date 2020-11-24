e6bfc2f1682804872e6cf1721308dd2e38380d71
David Van Horn <dvanhorn@cs.umd.edu>
2020-11-23 22:32:12 -0500

    Dodger.

---
+++ www/notes/dodger/ast.rkt
@@ -0,0 +1,16 @@
+#lang racket
+(provide Int Bool Char Prim If)
+
+;; type Expr =
+;; | (Int Integer)
+;; | (Bool Boolean)
+;; | (Char Character)
+;; | (Prim Op Expr)
+;; | (If Expr Expr Expr)
+;; type Op = 'add1 | 'sub1 | 'zero?
+;;         | 'char? | 'integer->char | 'char->integer
+(struct Int (i) #:prefab)
+(struct Bool (b) #:prefab)
+(struct Char (c) #:prefab)
+(struct Prim (p e) #:prefab)
+(struct If (e1 e2 e3) #:prefab)
