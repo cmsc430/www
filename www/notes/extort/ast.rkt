684102f8fe8613b768df31d390cd032e8c8aca92
David Van Horn <dvanhorn@cs.umd.edu>
2020-11-24 17:03:21 -0500

    Update extort to account for earlier langauges.

--- www/notes/extort/ast.rkt
+++ www/notes/extort/ast.rkt
@@ -1,13 +1,24 @@
 #lang racket
-(provide Int Bool Prim If)
+(provide Eof Int Bool Char Prim0 Prim1 If Begin)
 
 ;; type Expr =
+;; | (Eof)
 ;; | (Int Integer)
 ;; | (Bool Boolean)
-;; | (Prim Op Expr)
+;; | (Char Character)
+;; | (Prim0 Op0)
+;; | (Prim1 Op1 Expr)
 ;; | (If Expr Expr Expr)
-;; type Op = 'add1 | 'sub1 | 'zero?
-(struct Int (i) #:prefab)
-(struct Bool (b) #:prefab)
-(struct Prim (p e) #:prefab)
-(struct If (e1 e2 e3) #:prefab)
+;; | (Begin Expr Expr)
+;; type Op0 = 'read-byte
+;; type Op1 = 'add1 | 'sub1 | 'zero?
+;;          | 'char? | 'integer->char | 'char->integer
+;;          | 'write-byte | 'eof-object?
+(struct Eof   ()         #:prefab)
+(struct Int   (i)        #:prefab)
+(struct Bool  (b)        #:prefab)
+(struct Char  (c)        #:prefab)
+(struct Prim0 (p)        #:prefab)
+(struct Prim1 (p e)      #:prefab)
+(struct If    (e1 e2 e3) #:prefab)
+(struct Begin (e1 e2)    #:prefab)
--- www/notes/extort/test/compile-rand.rkt
+++ www/notes/extort/test/compile-rand.rkt
@@ -3,7 +3,8 @@
 
 (define (check-compiler e)
   (check-eqv? (interp e)
-              (asm-interp (compile e))))
+              (asm-interp (compile e))
+              e))
 
 (for ([i (in-range 500)])
   (check-compiler (random-expr)))
