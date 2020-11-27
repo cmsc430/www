f5eb8c0c05effd723a166fba268c9b4f029a81b2
David Van Horn <dvanhorn@cs.umd.edu>
2020-11-27 11:35:06 -0500

    Remove random testing for now.
    It's *so* slow.  Have got to find a better way to do this, otherwise
    build times are ridiculous.

--- www/notes/con/test/compile-rand.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-(require "../interp.rkt" "../compile.rkt" "../asm/interp.rkt" "../random.rkt" rackunit)
-
-(define (check-compiler e)
-  (check-eqv? (interp e)
-              (asm-interp (compile e))))
-
-(for ([i (in-range 500)])
-  (check-compiler (random-expr)))
--- www/notes/dupe/test/compile-rand.rkt
+++
@@ -1,5 +0,0 @@
-#lang racket
-(require "../correct.rkt" "../random.rkt" rackunit)
-
-(for ([i (in-range 500)])
-  (check-correctness (random-expr)))
