e7b564d08c06ddf9783de6f2e2c4c035c18af20b
David Van Horn <dvanhorn@cs.umd.edu>
2021-01-14 23:10:48 -0500

    Move staff to defns file.

--- www/defns.rkt
+++ www/defns.rkt
@@ -19,3 +19,8 @@
 (define elms-url "TBD")
 
 (define racket-version "7.9")
+
+(define staff
+  (list #;(list "Tasnim Kabir" "tkabir1@cs.umd.edu" "Friday 9am-11am (EST)")
+        #;(list "Alex Taber" "ataber@terpmail.umd.edu" "Tue/Thur 3:30-5:00 PM EST")
+        #;(list "Vyas Gupta" "vgupta13@terpmail.umd.edu" "M/W 2-3:30pm EST")))
