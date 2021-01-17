99f0fb42a0fe12cf4ec886ea3fd5f369b1d1230a
David Van Horn <dvanhorn@cs.umd.edu>
2021-01-17 01:30:10 -0500

    More checking in a86.

--- langs/a86/ast.rkt
+++ langs/a86/ast.rkt
@@ -1,50 +1,99 @@
 #lang racket
 (provide (all-defined-out))
 
-;; type Asm = [Listof Instruction]
-;; type Instruction =
-;; | (Label Symbol)
-;; | (Cal Symbol)
-;; | (Ret)
-;; | (Mov Arg Arg)
-;; | (Add Arg Arg)
-;; | (Sub Arg Arg)
-;; | (Cmp Arg Arg)
-;; | (Jmp Symbol)
-;; | (Je  Symbol)
-;; | (Jne Symbol)
-;; | (And Arg Arg)
-;; | (Or Arg Arg)
-;; | (Xor Arg Arg)
-;; | (Sal Arg Arg)
-;; | (Sar Arg Arg)
-;; | (Push Arg)
-;; | (Pop Arg)
-;; type Arg =
-;; | 'rax
-;; | Number
-;; | (Offset Reg Integer)
-(struct Label (x)     #:prefab)
-(struct Call  (x)     #:prefab)
-(struct Ret   ()      #:prefab)
-(struct Mov   (a1 a2) #:prefab)
-(struct Add   (a1 a2) #:prefab)
-(struct Sub   (a1 a2) #:prefab)
-(struct Cmp   (a1 a2) #:prefab)
-(struct Jmp   (x)     #:prefab)
-(struct Je    (x)     #:prefab)
-(struct Jne   (x)     #:prefab)
-(struct Jl    (x)     #:prefab)
-(struct Jg    (x)     #:prefab)
-(struct And   (a1 a2) #:prefab)
-(struct Or    (a1 a2) #:prefab)
-(struct Xor   (a1 a2) #:prefab)
-(struct Sal   (a1 a2) #:prefab)
-(struct Sar   (a1 a2) #:prefab)
-(struct Push  (a1)    #:prefab)
-(struct Pop   (a1)    #:prefab)
+(define (register? x)
+  (and (memq x '(rax rbx rcx rdx rbp rsp rsi rdi r8 r9 r10 r11 r12 r13 r14 r15)) #t))
 
-(struct Offset (r i)  #:prefab)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Guards
+
+(define check:symbol
+  (λ (x n)
+    (unless (symbol? x)
+      (error n "expects symbol; given ~v" x))
+    x))
+
+(define check:arith  
+  (λ (a1 a2 n)
+    (unless (register? a1)
+      (error n "expects register; given ~v" a1))
+    (unless (or (exact-integer? a2) (register? a2) (offset? a2))
+      (error n "expects exact integer, register, or offset; given ~v" a2))
+    (values a1 a2)))
+
+(define check:register
+  (λ (a1 n)
+    (unless (register? a1)
+      (error n "expects register; given ~v" a1))
+    a1))
+
+(define check:src-dest
+  (λ (a1 a2 n)
+    (unless (or (register? a1) (offset? a1))
+      (error n "expects register or offset; given ~v" a1))
+    (unless (or (register? a2) (offset? a2) (exact-integer? a2))
+      (error n "expects register, offset, or exact integer; given ~v" a2))
+    (when (and (offset? a1) (offset? a2))
+      (error n "cannot use two memory locations; given ~v, ~v" a1 a2))
+    (values a1 a2)))
+
+(define check:none
+  (λ (n) (values)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define-syntax-rule
+  (instruct Name (x ...) guard)
+  (begin (provide (struct-out Name))
+         (struct Name (x ...)
+           #:transparent
+           #:guard guard)))
+
+
+(instruct Label (x)     check:symbol)
+(instruct Call  (x)     check:symbol)
+(instruct Ret   ()      check:none)
+(instruct Mov   (a1 a2) check:src-dest)
+(instruct Add   (a1 a2) check:arith)
+(instruct Sub   (a1 a2) check:arith)
+
+;; FIXME
+(provide (struct-out Cmp))
+(struct Cmp (a1 a2) #:prefab)
+
+(instruct Jmp (x)     check:symbol)
+(instruct Je  (x)     check:symbol)
+(instruct Jne (x)     check:symbol)
+(instruct Jl  (x)     check:symbol)
+(instruct Jg  (x)     check:symbol)
+(instruct And (a1 a2) check:src-dest)
+(instruct Or  (a1 a2) check:src-dest)
+(instruct Xor (a1 a2) check:src-dest)
+
+;; FIXME
+(struct Sal (a1 a2) #:prefab)
+(struct Sar (a1 a2) #:prefab)
+
+(instruct Push (a1)
+          (λ (a1 n)
+            (unless (or (exact-integer? a1) (register? a1))
+              (error n "expects exact integer or register; given ~v" a1))
+            a1))
+
+(instruct Pop (a1)  check:register)
+
+(instruct Offset (r i)
+          (λ (r i n)
+            (unless (register? r)
+              (error n "expects register as first argument; given ~v" r))
+            (unless (exact-integer? i)
+              (error n "expects exact integer as second argument; given ~v" i))
+            (values r i)))
+
+(define offset? Offset?)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; (U Instruction Asm) ... -> Asm
 ;; Convenient for sequencing instructions or groups of instructions
@@ -55,3 +104,59 @@
                (cons x is)))
          '()
          xs))
+
+;; (U Instruction Asm) ... -> Asm
+;; Construct a "program", does some global well-formedness checking to help
+;; prevent confusing error messages as the nasm level
+(define (progn . xs)
+  (let ((p (apply seq xs)))
+    (check-unique-label-decls xs)
+    (check-label-targets-declared xs)
+    ;; anything else?
+    xs))
+
+;; Asm -> Void
+(define (check-unique-label-decls xs)
+  (let ((r (check-duplicates (label-decls xs))))
+    (when r
+      (error 'progn "duplicate label declaration found: ~v" r))))
+
+;; Asm -> (Listof Symbol)
+;; Compute all declared label names
+(define (label-decls asm)
+  (match asm
+    ['() '()]
+    [(cons (Label s) asm)
+     (cons s (label-decls asm))]
+    [(cons _ asm)
+     (label-decls asm)]))
+
+;; Asm -> (Listof Symbol)
+;; Compute all uses of label names
+(define (label-uses asm)
+  (match asm
+    ['() '()]
+    [(cons (Jmp (? symbol? s)) asm)
+     (cons s (label-uses asm))]
+    [(cons (Je (? symbol? s)) asm)
+     (cons s (label-uses asm))]
+    [(cons (Jne (? symbol? s)) asm)
+     (cons s (label-uses asm))]
+    [(cons (Jg (? symbol? s)) asm)
+     (cons s (label-uses asm))]
+    [(cons (Jl (? symbol? s)) asm)
+     (cons s (label-uses asm))]
+    [(cons (Call (? symbol? s)) asm)
+     (cons s (label-uses asm))]
+    [(cons _ asm)
+     (label-uses asm)]))
+
+
+;; Asm -> Void
+(define (check-label-targets-declared asm)
+  (let ((ds (apply set (label-decls asm)))
+        (us (apply set (label-uses asm))))
+
+    (let ((undeclared (set-subtract us ds)))
+      (unless (set-empty? undeclared)
+        (error 'progn "undeclared labels found: ~v" (set->list undeclared))))))
