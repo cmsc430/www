69459e78f13bd50209d9483193932e45975cc609
David Van Horn <dvanhorn@cs.umd.edu>
2021-01-18 13:27:03 -0500

    Use ffi for asm-interp; remove a86 'run-time'.

--- langs/a86/interp.rkt
+++ langs/a86/interp.rkt
@@ -1,23 +1,25 @@
 #lang racket
 (provide (all-defined-out))
-(require "printer.rkt" racket/runtime-path)
-(define-runtime-path dir ".")
+(require "printer.rkt" ffi/unsafe)
 
 ;; Asm -> Integer
-;; Interpret (by assemblying, linking, and exec'ing) x86-64 code
-;; Assume: starts with entry point run-time expects
+;; Interpret (by assemblying, linking, and loading) x86-64 code
+;; Assume: entry point is "entry"
 (define (asm-interp a)
-  (let* ((t.s (make-temporary-file "nasm~a.s"))
-         (t.run (path-replace-extension t.s #".run")))
-    (with-output-to-file t.s
-      #:exists 'truncate
-      (λ ()
-        (displayln (asm-string a))))
-    (system (format "(cd ~a && make -s ~a) 2>&1 >/dev/null" dir t.run))
-    ;(delete-file t.s)
-    (with-input-from-string
-        (with-output-to-string
-          (λ ()
-            (system (path->string t.run))
-            (delete-file t.run)))
-      read)))
+  (define t.s  (make-temporary-file "nasm~a.s"))
+  (define t.o  (path-replace-extension t.s #".o"))
+  (define t.so (path-replace-extension t.s #".so"))
+  (with-output-to-file t.s
+    #:exists 'truncate
+    (λ ()
+      (displayln (asm-string a))))
+
+  (system (format "nasm -f ~a ~a && gcc -shared ~a -o ~a"
+                  (if (eq? (system-type 'os) 'macosx) 'macho64 'elf64)
+                  t.s t.o t.so))
+
+  (define libt.so (ffi-lib t.so))
+  (delete-file t.s)
+  (delete-file t.o)
+  (delete-file t.so)
+  ((get-ffi-obj "entry" libt.so (_fun -> _int64))))
