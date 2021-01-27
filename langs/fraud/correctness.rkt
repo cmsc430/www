16debaea127c9a107d90bd4ce5b3d9a24271f708
David Van Horn <dvanhorn@cs.umd.edu>
2021-01-27 11:13:50 -0500

    Remove missed use of asm.

--- langs/fraud/correctness.rkt
+++ langs/fraud/correctness.rkt
@@ -1,11 +1,10 @@
 #lang racket
 (provide (all-defined-out))
-(require "interp.rkt"
-         "compile.rkt"
-         "asm/interp.rkt"
-         rackunit)
+(require "interp.rkt" "compile.rkt" "types.rkt" a86 rackunit)
  
 (define (check-compiler e)
-  (check-eqv? (asm-interp (compile e))
+  (check-eqv? (match (asm-interp (compile e))
+                ['err 'err]
+                [b (bits->value b)])
               (interp e)
               e))
