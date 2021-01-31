bef4e8bbd262a11c2d3eb73e0f3a629aaf0e8435
David Van Horn <dvanhorn@cs.umd.edu>
2021-01-30 23:19:56 -0500

    Revert "Fix stack discipline on Fraud"
    This reverts commit cc9fd2b7c5cc9adcb96da7482930cf23b5529484.
    
    Didn't mean to include the changes to iniquity in progress.

--- langs/iniquity/byte.c
+++ langs/iniquity/byte.c
@@ -1,18 +1,9 @@
 #include <stdio.h>
 #include <inttypes.h>
 #include "types.h"
-#include "runtime.h"
 
 int64_t read_byte(void) {
-  char c = getc(in);
-  return (c == EOF) ?
-    val_eof :
-    (int64_t)(c << int_shift);
-}
-
-int64_t peek_byte(void) {
-  char c = getc(in);
-  ungetc(c, in);
+  char c = getc(stdin);
   return (c == EOF) ?
     val_eof :
     (int64_t)(c << int_shift);
@@ -20,6 +11,6 @@ int64_t peek_byte(void) {
 
 int64_t write_byte(int64_t c) {
   int64_t codepoint = c >> int_shift;
-  putc((char) codepoint, out);
+  printf("%c", (char) codepoint);
   return 0;
 }
