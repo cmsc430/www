00753adcdc9b23539ba11d23e562863c489dfb93
David Van Horn <dvanhorn@cs.umd.edu>
2021-02-02 15:47:59 -0500

    Break out compile-op1 as own function; jump directly to raise_error.

--- langs/evildoer/compile.rkt
+++ langs/evildoer/compile.rkt
@@ -44,43 +44,47 @@
 ;; Op1 Expr -> Asm
 (define (compile-prim1 p e)
   (seq (compile-e e)
-       (match p
-         ['add1 (Add rax (value->bits 1))]
-         ['sub1 (Sub rax (value->bits 1))]
-         ['zero?
-          (let ((l1 (gensym)))
-            (seq (Cmp rax 0)
-                 (Mov rax val-true)
-                 (Je l1)
-                 (Mov rax val-false)
-                 (Label l1)))]
-         ['char?
-          (let ((l1 (gensym)))
-            (seq (And rax mask-char)
-                 (Xor rax type-char)
-                 (Cmp rax 0)
-                 (Mov rax val-true)
-                 (Je l1)
-                 (Mov rax val-false)
-                 (Label l1)))]
-         ['char->integer
-          (seq (Sar rax char-shift)
-               (Sal rax int-shift))]
-         ['integer->char
-          (seq (Sar rax int-shift)
-               (Sal rax char-shift)
-               (Xor rax type-char))]
-         ['eof-object?
-          (let ((l1 (gensym)))
-            (seq (Cmp rax val-eof)
-                 (Mov rax val-true)
-                 (Je l1)
-                 (Mov rax val-false)
-                 (Label l1)))]
-         ['write-byte
-          (seq (Mov rdi rax)
-               (Call 'write_byte)
-               (Mov rax val-void))])))
+       (compile-op1 p)))
+
+;; Op1 -> Asm
+(define (compile-op1 p)
+  (match p
+    ['add1 (Add rax (value->bits 1))]
+    ['sub1 (Sub rax (value->bits 1))]
+    ['zero?
+     (let ((l1 (gensym)))
+       (seq (Cmp rax 0)
+            (Mov rax val-true)
+            (Je l1)
+            (Mov rax val-false)
+            (Label l1)))]
+    ['char?
+     (let ((l1 (gensym)))
+       (seq (And rax mask-char)
+            (Xor rax type-char)
+            (Cmp rax 0)
+            (Mov rax val-true)
+            (Je l1)
+            (Mov rax val-false)
+            (Label l1)))]
+    ['char->integer
+     (seq (Sar rax char-shift)
+          (Sal rax int-shift))]
+    ['integer->char
+     (seq (Sar rax int-shift)
+          (Sal rax char-shift)
+          (Xor rax type-char))]
+    ['eof-object?
+     (let ((l1 (gensym)))
+       (seq (Cmp rax val-eof)
+            (Mov rax val-true)
+            (Je l1)
+            (Mov rax val-false)
+            (Label l1)))]
+    ['write-byte
+     (seq (Mov rdi rax)
+          (Call 'write_byte)
+          (Mov rax val-void))]))
 
 ;; Expr Expr Expr -> Asm
 (define (compile-if e1 e2 e3)
