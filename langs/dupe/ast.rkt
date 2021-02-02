b856253ae387ebcee11d4978e3b9cf867948e89b
David Van Horn <dvanhorn@cs.umd.edu>
2021-02-02 15:41:53 -0500

    Formatting ast file consistently.

--- langs/dupe/ast.rkt
+++ langs/dupe/ast.rkt
@@ -7,7 +7,7 @@
 ;; | (Prim1 Op Expr)
 ;; | (If Expr Expr Expr)
 ;; type Op = 'add1 | 'sub1 | 'zero?
-(struct Int (i) #:prefab)
-(struct Bool (b) #:prefab)
-(struct Prim1 (p e) #:prefab)
+(struct Int (i)       #:prefab)
+(struct Bool (b)      #:prefab)
+(struct Prim1 (p e)   #:prefab)
 (struct If (e1 e2 e3) #:prefab)
