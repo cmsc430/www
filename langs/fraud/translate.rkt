3e11ab3512e8fd68a9914eb3a566e122edd8aac2
David Van Horn <dvanhorn@cs.umd.edu>
2021-02-02 10:55:21 -0500

    Update interpreter and lexical address translation.

--- langs/fraud/translate.rkt
+++ langs/fraud/translate.rkt
@@ -3,14 +3,18 @@
 (require "ast.rkt")
 
 ;; type IExpr =
+;; | (Eof)
 ;; | (Int Integer)
 ;; | (Bool Boolean)
-;; | (Prim Op Expr)
-;; | (If Expr Expr Expr)
-;; | (Let '_ Expr Expr)
+;; | (Char Character)
+;; | (Prim0 Op0)
+;; | (Prim1 Op1 IExpr)
+;; | (Prim2 Op2 IExpr IExpr)
+;; | (If IExpr IExpr IExpr)
+;; | (Begin IExpr IExpr)
+;; | (Let '_ IExpr IExpr)
 ;; | (Var Addr)
 ;; type Addr = Natural
-;; type prim = 'add1 | 'sub1 | 'zero?
 
 ;; type LEnv = (Listof Id)
 
@@ -21,20 +25,30 @@
 ;; Expr LEnv -> IExpr
 (define (translate-e e r)
   (match e
-    [(Var x)
-     (Var (lexical-address x r))]
-    [(Int i) e]
+    [(Eof)    e]
+    [(Int i)  e]
     [(Bool b) e]   
-    [(Prim1 p e)
-     (Prim1 p (translate-e e r))]
+    [(Char c) e]
+    [(Prim0 p) e]
+    [(Prim1 p e0)
+     (Prim1 p (translate-e e0 r))]
+    [(Prim2 p e0 e1)
+     (Prim2 p
+            (translate-e e0 r)
+            (translate-e e1 r))]
     [(If e0 e1 e2)
      (If (translate-e e0 r)
          (translate-e e1 r)
          (translate-e e2 r))]
-    [(Let x e1 e2)
+    [(Begin e0 e1)
+     (Begin (translate-e e0 r)
+            (translate-e e1 r))]               
+    [(Let x e0 e1)
      (Let '_
-          (translate-e e1 r)
-          (translate-e e2 (cons x r)))]))
+          (translate-e e0 r)
+          (translate-e e1 (cons x r)))]
+    [(Var x)
+     (Var (lexical-address x r))]))
 
 ;; Id LEnv -> Addr
 (define (lexical-address x r)
