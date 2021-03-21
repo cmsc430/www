593ff2fada8dab319ca979811340bf692335e50b
José Manuel Calderón Trilla <jmct@umd.edu>
2021-03-21 16:58:10 -0400

    Experiment with assuming jump-threading

--- langs/dupe/main.c
+++ langs/dupe/main.c
@@ -10,7 +10,25 @@ int main(int argc, char** argv) {
   return 0;
 }
 
-void print_result(int64_t result) {
+typedef struct {
+  int64_t tag;
+  int64_t val;
+} Val;
+
+Val decode(int64_t word) {
+  if (int_type_tag == (int_type_mask & word)) {
+    return (Val){0,word >> int_shift};
+  } else {
+    switch (word) {
+    case val_true:
+      return (Val){1,1}; break;
+    case val_false:
+      return (Val){1,0}; break;
+    }
+  }
+}
+
+void print_result_old(int64_t result) {
   if (int_type_tag == (int_type_mask & result)) {
     printf("%" PRId64 "\n", result >> int_shift);
   } else {
@@ -20,5 +38,19 @@ void print_result(int64_t result) {
     case val_false:
       printf("#f\n"); break;
     }
+  }
+}
+
+void print_result_new(int64_t result) {
+  Val d = decode(result);
+  switch (d.tag) {
+    case 0:
+      printf("%" PRId64 "\n", d.val); break;
+    case 1:
+      if (d.val) {
+        printf("#t\n");
+      } else {
+        printf("#f\n");
+      } break;
   }
 }
--- langs/dupe/main.rs
+++ langs/dupe/main.rs
@@ -4,6 +4,38 @@
 mod types;
 use types::*;
 
+#[derive(Debug)]
+enum Val {
+  Int(i64),
+  Bool(bool),
+}
+
+impl Val {
+  #[inline(always)]
+  fn decode(word: i64) -> Val {
+    // Is it an integer?
+    if INT_TYPE_TAG == (INT_TYPE_MASK & word) {
+      Val::Int(word >> INT_TYPE_MASK as i64)
+    } else {
+      match word {
+        VAL_TRUE  => Val::Bool(true),
+        VAL_FALSE => Val::Bool(false),
+        _         => print_panic(),
+      }
+    }
+  }
+}
+
+#[no_mangle]
+fn printresult(res: i64) {
+  match Val::decode(res) {
+    Val::Int(ires)   => print_int(ires),
+    Val::Bool(true)  => print_true(),
+    Val::Bool(false) => print_false(),
+  }
+}
+
+
 extern "C" {
   fn entry() -> i64;
 }
@@ -16,22 +48,50 @@ extern "C" {
 #[no_mangle]
 fn main(argc: isize, argv: *const *const u8) -> isize {
   let res: i64 = unsafe { entry() };
-  printresult(res);
+  println!("{:?}", Val::decode(res));
   return 0;
 }
 
-fn printresult(res: i64) {
-
+#[no_mangle]
+fn printresult_old(res: i64) {
   // Is it an integer?
   if INT_TYPE_TAG == (INT_TYPE_MASK & res) {
     let ires = res >> INT_TYPE_MASK as i64;
-    println!("{}", ires);
+    print_int(ires);
   } else {
     match res {
-      VAL_TRUE  => println!("#t"),
-      VAL_FALSE => println!("#f"),
-      _         => println!("PANIC: malformed value"),
+      VAL_TRUE  => print_true(),
+      VAL_FALSE => print_false(),
+      _         => { print_panic(); },
     };
   }
 }
 
+#[no_mangle]
+fn printresult_new(res: i64) {
+  match Val::decode(res) {
+    Val::Int(ires)   => print_int(ires),
+    Val::Bool(true)  => print_true(),
+    Val::Bool(false) => print_false(),
+  }
+}
+
+#[inline(never)]
+fn print_int(i: i64) {
+  println!("{}", i);
+}
+
+#[inline(never)]
+fn print_true() {
+  println!("#t");
+}
+
+#[inline(never)]
+fn print_false() {
+  println!("#f");
+}
+
+#[inline(never)]
+fn print_panic() -> Val {
+  panic!("PANIC: malformed value")
+}
