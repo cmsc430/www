531f7af8859d08de55fcc2b4f94b877673a38d50
José Manuel Calderón Trilla <jmct@umd.edu>
2021-04-02 01:21:35 -0400

    WIP: I am trying to figure out the test-runner.
    Currently we'll get this error when running the test suite:
    
    ```
    match: no matching clause for '(#<cpointer> . 112)
      location...:
       test/all.rkt:99:33
    ```
    
    Line 99 in test/all.rkt is
    
    ```
    (test-runner (λ (e) (unload/free (match (asm-interp (compile (parse e)))))))
    ```
    
    I had changed this line because I was seeing a very similar when the line was
    as follows:
    
    ```
    (test-runner (λ (e) (match (asm-interp (compile (parse e)))
                          ['err 'err]
                          [bs (bits->value bs)])))
    ```
    
    I figured that the error was because Racket was getting confused with the cons
    value that Hustle returns, and switched it to unload/free because unload/free
    does the right thing with chasing pointers...

--- langs/hustle/interp-io.rkt
+++ langs/hustle/interp-io.rkt
@@ -1,12 +1,12 @@
 #lang racket
-(provide make-interp/io)
+(provide interp/io)
+(require "interp.rkt")
 
-;; (Expr -> Answer) -> (Expr String -> String)
+;; Expr String -> (Cons Value String)
 ;; Interpret e with given string as input,
 ;; collect output as string (including printed result)
-(define (make-interp/io interp)
-  (λ (e in)
-    (parameterize ((current-output-port (open-output-string))
-                   (current-input-port  (open-input-string in)))
+(define (interp/io e input)
+  (parameterize ((current-output-port (open-output-string))
+                 (current-input-port  (open-input-string input)))
       (cons (interp e)
-            (get-output-string (current-output-port))))))
+            (get-output-string (current-output-port)))))
