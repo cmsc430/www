822909a4775a930ae214def7416f536b1534c4e6
David Van Horn <dvanhorn@cs.umd.edu>
2021-06-28 13:54:37 -0400

    Update for Fall 2021.

--- www/defns.rkt
+++ www/defns.rkt
@@ -2,10 +2,10 @@
 (provide (all-defined-out))
 (require scribble/core scribble/html-properties scribble/manual) 
 
-(define prof (link "https://lemonidas.github.io/" "Leonidas Lampropoulos"))
-(define prof-email "leonidas@umd.edu")
+(define prof (link "https://www.cs.umd.edu/~dvanhorn/" "David Van Horn"))
+(define prof-email "dvanhorn@cs.umd.edu")
 
-(define semester "spring")
+(define semester "fall")
 (define year "2021")
 (define courseno "CMSC 430")
 
@@ -22,7 +22,4 @@
 
 (define staff
   (list (list (link "http://jmct.cc/" "José Manuel Calderón Trilla") "jmct@umd.edu" "-")
-        (list "Deena Postol" "dpostol@umd.edu" "Mon 9-11am EST")
-        (list "Tasnim Kabir" "tkabir1@cs.umd.edu" "Tue 9-11am EST")        
-        (list "Temur Saidkhodjaev" "temurson@terpmail.umd.edu" "Wed 11am-1pm EST")
         (list "Vyas Gupta" "vgupta13@terpmail.umd.edu" "Thu 3.30-5.30pm EST")))
