eb978bb9f71b642dcae6ebc4cc31db88debdfaa0
David Van Horn <dvanhorn@cs.umd.edu>
2021-09-20 10:03:38 -0400

    Simplify parser.

--- langs/dodger/parse.rkt
+++ langs/dodger/parse.rkt
@@ -4,18 +4,15 @@
 
 ;; S-Expr -> Expr
 (define (parse s)
-  (cond
-    [(integer? s) (Int s)]
-    [(boolean? s) (Bool s)]
-    [(char? s)    (Char s)]
-    [else
-     (match s
-       [(list 'add1 e)  (Prim1 'add1 (parse e))]
-       [(list 'sub1 e)  (Prim1 'sub1 (parse e))]
-       [(list 'zero? e) (Prim1 'zero? (parse e))]
-       [(list 'char? e) (Prim1 'char? (parse e))]
-       [(list 'integer->char e) (Prim1 'integer->char (parse e))]
-       [(list 'char->integer e) (Prim1 'char->integer (parse e))]
-       [(list 'if e1 e2 e3)
-        (If (parse e1) (parse e2) (parse e3))]
-       [_ (error "Parse error")])]))
+  (match s
+    [(? integer?) (Int s)]
+    [(? boolean?) (Bool s)]
+    [(? char?)    (Char s)]
+    [(list (? op1? o) e)  (Prim1 o (parse e))]
+    [(list 'if e1 e2 e3)
+     (If (parse e1) (parse e2) (parse e3))]
+    [_ (error "Parse error")]))
+
+;; Any -> Boolean
+(define (op1? x)
+  (memq x '(add1 sub1 zero? char? integer->char char->integer)))
