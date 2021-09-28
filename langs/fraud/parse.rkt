de058137c191f0ea51faa0518f2d4efdf0aa3d7e
David Van Horn <dvanhorn@cs.umd.edu>
2021-09-28 13:21:05 -0400

    Update Fraud parse to be consistent with priors.

--- langs/fraud/parse.rkt
+++ langs/fraud/parse.rkt
@@ -4,32 +4,28 @@
 
 ;; S-Expr -> Expr
 (define (parse s)
-  (cond
-    [(integer? s) (Int s)]
-    [(boolean? s) (Bool s)]
-    [(char? s)    (Char s)]
-    [(symbol? s)  (match s
-                    ['eof (Eof)]
-                    [_ (Var s)])]
-    [else
-     (match s
-       ['eof                    (Eof)]
-       [(list 'read-byte)       (Prim0 'read-byte)]
-       [(list 'peek-byte)       (Prim0 'peek-byte)]
-       [(list 'void)            (Prim0 'void)]
-       [(list 'add1 e)          (Prim1 'add1 (parse e))]
-       [(list 'sub1 e)          (Prim1 'sub1 (parse e))]
-       [(list 'zero? e)         (Prim1 'zero? (parse e))]
-       [(list 'char? e)         (Prim1 'char? (parse e))]
-       [(list 'write-byte e)    (Prim1 'write-byte (parse e))]
-       [(list 'eof-object? e)   (Prim1 'eof-object? (parse e))]
-       [(list 'integer->char e) (Prim1 'integer->char (parse e))]
-       [(list 'char->integer e) (Prim1 'char->integer (parse e))]
-       [(list 'begin e1 e2)     (Begin (parse e1) (parse e2))]
-       [(list '+ e1 e2)         (Prim2 '+ (parse e1) (parse e2))]
-       [(list '- e1 e2)         (Prim2 '- (parse e1) (parse e2))]       
-       [(list 'if e1 e2 e3)
-        (If (parse e1) (parse e2) (parse e3))]
-       [(list 'let (list (list (? symbol? x) e1)) e2)
-        (Let x (parse e1) (parse e2))]
-       [_ (error "Parse error" s)])]))
+  (match s
+    [(? integer?)            (Int s)]
+    [(? boolean? s)          (Bool s)]
+    [(? char? s)             (Char s)]
+    ['eof                    (Eof)]
+    [(? symbol? s)           (Var s)]    
+    [(list (? op0? o))       (Prim0 o)]
+    [(list (? op1? o) e)     (Prim1 o (parse e))]
+    [(list (? op2? o) e1 e2) (Prim2 o (parse e1) (parse e2))]
+    [(list 'begin e1 e2)     (Begin (parse e1) (parse e2))]      
+    [(list 'if e1 e2 e3)
+     (If (parse e1) (parse e2) (parse e3))]
+    [(list 'let (list (list (? symbol? x) e1)) e2)
+     (Let x (parse e1) (parse e2))]
+    [_ (error "Parse error" s)]))
+
+
+;; Any -> Boolean
+(define (op0? x)
+  (memq x '(read-byte peek-byte)))
+(define (op1? x)
+  (memq x '(add1 sub1 zero? char? integer->char char->integer
+                 write-byte eof-object?)))
+(define (op2? x)
+  (memq x '(+ -)))
