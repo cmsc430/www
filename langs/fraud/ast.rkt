aeab282d091855b1d86e107fc031ae4523e4212e
David Van Horn <dvanhorn@cs.umd.edu>
2021-10-06 16:22:54 -0400

    Typo is Fraud ast.

--- langs/fraud/ast.rkt
+++ langs/fraud/ast.rkt
@@ -8,7 +8,7 @@
 ;; | (Char Character)
 ;; | (Prim0 Op0)
 ;; | (Prim1 Op1 Expr)
-;; | (Prim2 Op2 Op2 Expr)
+;; | (Prim2 Op2 Expr Expr)
 ;; | (If Expr Expr Expr)
 ;; | (Begin Expr Expr)
 ;; | (Let Id Expr Expr)
