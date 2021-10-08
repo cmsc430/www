0ba7949bada3f25cf3b17fdcf9f662a5f1271022
David Van Horn <dvanhorn@cs.umd.edu>
2021-10-08 13:26:50 -0400

    Touch up contract in heap code.

--- langs/hustle/heap.rkt
+++ langs/hustle/heap.rkt
@@ -1,17 +1,17 @@
 #lang racket
 (provide alloc-box alloc-cons alloc-str heap-ref heap-set)
 
-;; Value* Heap -> Answer
+;; Value* Heap -> Answer*
 (define (alloc-box v h)
   (cons (cons v h)
         (list 'box (length h))))
 
-;; Value* Value* Heap -> Answer
+;; Value* Value* Heap -> Answer*
 (define (alloc-cons v1 v2 h)
   (cons (cons v2 (cons v1 h))
         (list 'cons (length h))))
 
-;; String Heap -> Answer
+;; String Heap -> Answer*
 (define (alloc-str s h)
   (cons (append (reverse (string->list s)) (list (string-length s)) h)
         (list 'str (length h))))
