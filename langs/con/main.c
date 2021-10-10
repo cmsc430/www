06df5aa7118f8db28be069032e47396512da36cb
David Van Horn <dvanhorn@cs.umd.edu>
2021-10-10 17:36:47 -0400

    Set up Con like later langs.

--- langs/con/main.c
+++ langs/con/main.c
@@ -2,9 +2,19 @@
 #include <inttypes.h>
 
 int64_t entry();
+void print_result(int64_t);
 
-int main(int argc, char** argv) {
-  int64_t result = entry();
-  printf("%" PRId64 "\n", result);
+int main(int argc, char** argv)
+{
+  int64_t result;
+
+  result = entry();
+  print_result(result);
   return 0;
+}
+
+void print_result(int64_t x)
+{
+  printf("%" PRId64, x);
+  printf("\n");
 }
