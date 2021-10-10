86df356edbda2f3c36aaa1beff0785e47b7d9950
David Van Horn <dvanhorn@cs.umd.edu>
2021-10-10 17:50:14 -0400

    Split out print in Dupe runtime.

--- langs/dupe/main.c
+++ langs/dupe/main.c
@@ -1,9 +1,7 @@
-#include <stdio.h>
-#include <inttypes.h>
 #include "values.h"
+#include "print.h"
 
 val_t entry();
-void print_result(val_t);
 
 int main(int argc, char** argv)
 {
@@ -12,19 +10,4 @@ int main(int argc, char** argv)
   result = entry();
   print_result(result);
   return 0;
-}
-
-void print_result(val_t x)
-{
-  switch (val_typeof(x)) {
-  case T_INT:
-    printf("%" PRId64, val_unwrap_int(x));
-    break;
-  case T_BOOL:    
-    printf(val_unwrap_bool(x) ? "#t" : "#f");
-    break;
-  case T_INVALID:
-    printf("internal error");
-  }
-  printf("\n");
 }
