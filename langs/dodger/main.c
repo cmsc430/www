0e3d7e8d49106fa37fdaf4c33c30096406fc86e8
David Van Horn <dvanhorn@cs.umd.edu>
2021-10-10 20:15:15 -0400

    print treatment for Dodger.

--- langs/dodger/main.c
+++ langs/dodger/main.c
@@ -1,34 +1,13 @@
-#include <stdio.h>
-#include <inttypes.h>
 #include "values.h"
+#include "print.h"
 
 val_t entry();
-void print_result(val_t);
-void print_char(val_char_t);
 
 int main(int argc, char** argv)
 {
   val_t result;
 
   result = entry();
-  print_result(entry());
+  print_result(result);
   return 0;
-}
-
-void print_result(val_t x)
-{
-  switch (val_typeof(x)) {
-  case T_INT:
-    printf("%" PRId64, val_unwrap_int(x));
-    break;
-  case T_BOOL:    
-    printf(val_unwrap_bool(x) ? "#t" : "#f");
-    break;
-  case T_CHAR:
-    print_char(val_unwrap_char(x));    
-    break;
-  case T_INVALID:
-    printf("internal error");
-  }
-  printf("\n");
 }
