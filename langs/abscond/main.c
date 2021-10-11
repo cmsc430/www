486898b9e02d662f7cdf246a27e9085ee87043c5
David Van Horn <dvanhorn@cs.umd.edu>
2021-10-10 21:58:21 -0400

    print treatment for Abscond.

--- langs/abscond/main.c
+++ langs/abscond/main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <inttypes.h>
+#include "print.h"
 
 int64_t entry();
 
-int main(int argc, char** argv) {
-  int64_t result = entry();
-  printf("%" PRId64 "\n", result);
+int main(int argc, char** argv)
+{
+  int64_t result;
+
+  result = entry();
+  print_result(result);
+  putchar('\n');
   return 0;
 }
