61b73f46ed4ae114111f948ee5f3249e7e1a3894
David Van Horn <dvanhorn@cs.umd.edu>
2021-10-10 20:32:30 -0400

    print treatment for Extort.

--- langs/extort/main.c
+++ langs/extort/main.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-#include <inttypes.h>
 #include <stdlib.h>
 #include "values.h"
+#include "print.h"
 #include "runtime.h"
 
 FILE* in;
 FILE* out;
 void (*error_handler)();
-
-void print_result(val_t);
-void print_char(val_char_t);
 
 void error_exit()
 {
@@ -36,26 +33,4 @@ int main(int argc, char** argv)
     putchar('\n');
 
   return 0;
-}
-
-void print_result(val_t x)
-{
-  switch (val_typeof(x)) {
-  case T_INT:
-    printf("%" PRId64, val_unwrap_int(x));
-    break;
-  case T_BOOL:    
-    printf(val_unwrap_bool(x) ? "#t" : "#f");
-    break;
-  case T_CHAR:
-    print_char(val_unwrap_char(x));    
-    break;
-  case T_EOF:
-    printf("#<eof>");
-    break;
-  case T_VOID:
-    break;    
-  case T_INVALID:
-    printf("internal error");
-  }
 }
