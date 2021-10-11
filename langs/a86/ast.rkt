b1f63a9883c7d41e772342d71e301ad304945c66
David Van Horn <dvanhorn@cs.umd.edu>
2021-10-11 12:02:55 -0400

    Add eax register.

--- langs/a86/ast.rkt
+++ langs/a86/ast.rkt
@@ -138,7 +138,7 @@
 (define offset? Offset?)
 
 (define (register? x)
-  (and (memq x '(rax rbx rcx rdx rbp rsp rsi rdi r8 r9 r10 r11 r12 r13 r14 r15))
+  (and (memq x '(eax rax rbx rcx rdx rbp rsp rsi rdi r8 r9 r10 r11 r12 r13 r14 r15))
        #t))
 
 (define (label? x)
