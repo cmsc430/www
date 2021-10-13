a054bde219d07f93f4b0c806fd052abdd5839b4f
David Van Horn <dvanhorn@cs.umd.edu>
2021-10-13 00:13:29 -0400

    Add ast and parser from Iniquity to Jig.

--- langs/jig/ast.rkt
+++ langs/jig/ast.rkt
@@ -12,58 +12,40 @@
 ;;           | (Int Integer)
 ;;           | (Bool Boolean)
 ;;           | (Char Character)
+;;           | (Str String)
 ;;           | (Prim0 Op0)
 ;;           | (Prim1 Op1 Expr)
-;;           | (Prim1 Op2 Op2 Expr)
+;;           | (Prim2 Op2 Expr Expr)
+;;           | (Prim3 Op3 Expr Expr Expr)
 ;;           | (If Expr Expr Expr)
 ;;           | (Begin Expr Expr)
 ;;           | (Let Id Expr Expr)
 ;;           | (Var Id)
 ;;           | (App Id (Listof Expr))
 ;; type Id   = Symbol
-;; type Op0  = 'read-byte | 'void | 'collect-garbage
+;; type Op0  = 'read-byte
 ;; type Op1  = 'add1 | 'sub1 | 'zero?
 ;;           | 'char? | 'integer->char | 'char->integer
 ;;           | 'write-byte | 'eof-object?
 ;;           | 'box | 'car | 'cdr | 'unbox
-;;           | 'empty?
-;; type Op2  = '+ | '- | 'eq?
+;;           | 'empty? | 'cons? | 'box?
+;;           | 'vector? | vector-length
+;; type Op2  = '+ | '-
 ;;           | 'cons
+;;           | 'make-vector | 'vector-ref
+;; type Op3  = 'vector-set!
 (struct Eof   ()           #:prefab)
 (struct Empty ()           #:prefab)
 (struct Int   (i)          #:prefab)
 (struct Bool  (b)          #:prefab)
 (struct Char  (c)          #:prefab)
+(struct Str   (s)          #:prefab)
 (struct Prim0 (p)          #:prefab)
 (struct Prim1 (p e)        #:prefab)
 (struct Prim2 (p e1 e2)    #:prefab)
+(struct Prim3 (p e1 e2 e3) #:prefab)
 (struct If    (e1 e2 e3)   #:prefab)
 (struct Begin (e1 e2)      #:prefab)
 (struct Let   (x e1 e2)    #:prefab)
 (struct Var   (x)          #:prefab)
 (struct App   (f es)       #:prefab)
-
-
-;; Helper functions
-
-;; Does an Expr represent an immediate (i.e. flat) value?
-;; Expr -> Bool
-(define (imm? e)
-  (match e
-    [(Int i)  #t]
-    [(Bool b) #t]
-    [(Char c) #t]
-    [(Eof)    #t]
-    [(Empty)  #t]
-    [_        #f]))
-
-;; Get the 'actual' value out of an immediate.
-;; Expr -> Imm
-(define (get-imm e)
-  (match e
-    [(Int i)  i]
-    [(Bool b) b]
-    [(Char c) c]
-    [(Eof)    eof]
-    [(Empty)  '()]
-    [_        (error (~a "get-imm: " e " is not an immedate!"))]))
