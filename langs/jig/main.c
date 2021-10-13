983fd521d967e8f9283ceaa593ed2565222fb20c
David Van Horn <dvanhorn@cs.umd.edu>
2021-10-13 00:15:00 -0400

    Bring forward run-time system from Iniquity to Jig.

--- langs/jig/main.c
+++ langs/jig/main.c
@@ -1,81 +1,40 @@
 #include <stdio.h>
-#include <inttypes.h>
 #include <stdlib.h>
-#include "types.h"
+#include "values.h"
+#include "print.h"
 #include "runtime.h"
 
 FILE* in;
 FILE* out;
 void (*error_handler)();
-int64_t *heap;
+val_t *heap;
 
-void print_result(int64_t);
-
-void error_exit() {
+void error_exit()
+{
   printf("err\n");
   exit(1);
 }
 
-void raise_error() {
+void raise_error()
+{
   return error_handler();
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
   in = stdin;
   out = stdout;
   error_handler = &error_exit;
   heap = malloc(8 * heap_size);
-  int64_t result = entry(heap);
-  // See if we need to print the initial tick
-  if (cons_type_tag == (ptr_type_mask & result)) printf("'");
+
+  val_t result;
+
+  result = entry(heap);
+
   print_result(result);
-  if (result != val_void) printf("\n");
+  if (val_typeof(result) != T_VOID)
+    putchar('\n');
+
   free(heap);
   return 0;
-}
-
-void print_char(int64_t);
-void print_cons(int64_t);
-
-void print_result(int64_t result) {
-  if (cons_type_tag == (ptr_type_mask & result)) {
-    printf("(");
-    print_cons(result);
-    printf(")");
-  } else if (box_type_tag == (ptr_type_mask & result)) {
-    printf("#&");
-    print_result (*((int64_t *)(result ^ box_type_tag)));
-  } else if (int_type_tag == (int_type_mask & result)) {
-    printf("%" PRId64, result >> int_shift);
-  } else if (char_type_tag == (char_type_mask & result)) {
-    print_char(result);
-  } else {
-    switch (result) {
-    case val_true:
-      printf("#t"); break;
-    case val_false:
-      printf("#f"); break;
-    case val_eof:
-      printf("#<eof>"); break;
-    case val_empty:
-      printf("()"); break;
-    case val_void:
-      /* nothing */ break;
-    }
-  }  
-}
-
-void print_cons(int64_t a) {  
-  int64_t car = *((int64_t *)((a + 8) ^ cons_type_tag));
-  int64_t cdr = *((int64_t *)((a + 0) ^ cons_type_tag));
-  print_result(car);
-  if (cdr == val_empty) {
-    // nothing
-  } else if (cons_type_tag == (ptr_type_mask & cdr)) {
-    printf(" ");
-    print_cons(cdr);
-  } else {
-    printf(" . ");
-    print_result(cdr);
-  }
 }
--- langs/jig/types.h
+++ langs/jig/types.h
@@ -1,3 +1,6 @@
+#ifndef TYPES_H
+#define TYPES_H
+
 /*
   Bit layout of values
 
@@ -16,9 +19,10 @@
 */
 #define imm_shift        3
 #define ptr_type_mask    ((1 << imm_shift) - 1)
-#define ptr_addr_mask    ~ptr_type_mask
 #define box_type_tag     1
 #define cons_type_tag    2
+#define vect_type_tag    3
+#define str_type_tag     4
 #define int_shift        (1 + imm_shift)
 #define int_type_mask    ((1 << int_shift) - 1)
 #define int_type_tag     (0 << (int_shift - 1))
@@ -32,3 +36,5 @@
 #define val_eof   ((2 << char_shift) | nonchar_type_tag)
 #define val_void  ((3 << char_shift) | nonchar_type_tag)
 #define val_empty ((4 << char_shift) | nonchar_type_tag)
+
+#endif
