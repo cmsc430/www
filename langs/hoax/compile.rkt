4b29ab348d898fdb1a3a4828e549a04bddf10b13
David Van Horn <dvanhorn@cs.umd.edu>
2021-10-20 07:44:55 -0400

    Stack align overhaul for Hoax.

--- langs/hoax/compile.rkt
+++ langs/hoax/compile.rkt
@@ -22,7 +22,7 @@
         (compile-e e '())
         (Ret)
         (Label 'raise_error_align)
-        (Sub rsp 8)
+        (Or rsp 8)
         (Jmp 'raise_error)))
 
 ;; Expr CEnv -> Asm
@@ -76,19 +76,19 @@
 
 ;; Op0 CEnv -> Asm
 (define (compile-prim0 p c)
-  (compile-op0 p c))
+  (compile-op0 p))
 
 ;; Op1 Expr CEnv -> Asm
 (define (compile-prim1 p e c)
   (seq (compile-e e c)
-       (compile-op1 p c)))
+       (compile-op1 p)))
 
 ;; Op2 Expr Expr CEnv -> Asm
 (define (compile-prim2 p e1 e2 c)
   (seq (compile-e e1 c)
        (Push rax)
        (compile-e e2 (cons #f c))
-       (compile-op2 p c)))
+       (compile-op2 p)))
 
 ;; Op3 Expr Expr Expr CEnv -> Asm
 (define (compile-prim3 p e1 e2 e3 c)
@@ -97,7 +97,7 @@
        (compile-e e2 (cons #f c))
        (Push rax)
        (compile-e e3 (cons #f (cons #f c)))
-       (compile-op3 p c)))
+       (compile-op3 p)))
 
 ;; Expr Expr Expr CEnv -> Asm
 (define (compile-if e1 e2 e3 c)
