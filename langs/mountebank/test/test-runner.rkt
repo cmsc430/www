cea57292fe5d10056ef42af3a583917232cf53e0
David Van Horn <dvanhorn@cs.umd.edu>
2021-11-16 11:20:58 -0500

    Paren.

--- langs/mountebank/test/test-runner.rkt
+++ langs/mountebank/test/test-runner.rkt
@@ -317,7 +317,7 @@
   (check-equal? (run '(match (cons '+ (cons 1 (cons 2 '())))
                         [(cons '+ (cons x (cons y '())))
                          (+ x y)]))
-                3))
+                3)
 
   ;; Mountebank examples  
   (check-equal? (run '#())
