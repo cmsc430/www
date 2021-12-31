bf745c7dc1ef4ac41baef5e23b98503c37a764b8
David Van Horn <dvanhorn@cs.umd.edu>
2021-12-31 09:21:58 -0500

    Fix compile-file for iniquity and later.

--- langs/neerdowell/compile-file.rkt
+++ langs/neerdowell/compile-file.rkt
@@ -1,6 +1,6 @@
 #lang racket
 (provide main)
-(require "parse.rkt" "compile.rkt" a86/printer)
+(require "parse.rkt" "compile.rkt" "read-all.rkt" a86/printer)
 
 ;; String -> Void
 ;; Compile contents of given file name,
@@ -9,5 +9,5 @@
   (let ((p (open-input-file fn)))
     (begin
       (read-line p) ; ignore #lang racket line
-      (asm-display (compile (parse (read p))))
+      (asm-display (compile (parse (read-all p))))
       (close-input-port p))))
