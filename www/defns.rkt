215ac2322d77bb6f55463eb1faef7f7fabdbcdbc
dmanikt <dmanikt@gmail.com>
2022-03-09 11:23:56 -0500

    fix TA name

--- www/defns.rkt
+++ www/defns.rkt
@@ -22,7 +22,7 @@
 (define racket-version "8.3")
 
 (define staff
-  (list (list "Maniktala Dhruv" "dmanikt@umd.edu" "M/Tu 15:30-16:30 AVW 4160")
+  (list (list "Dhruv Maniktala" "dmanikt@umd.edu" "M/Tu 15:30-16:30 AVW 4160")
         (list "Benjamin Glover Quiring" "bquiring@umd.edu" "W 16:30-18:30 AVW 4160")
         (list "William Chung" "wchung1@terpmail.umd.edu" "Thu 14:00-16:00 AVW 4160")))
 
