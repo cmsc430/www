0e678fae91125bfaa76c92b0daebb44d6fa833af
David Van Horn <dvanhorn@cs.umd.edu>
2022-12-04 23:47:33 -0500

    Remove outlaw from default tests for langs (but make sure to test in CI).

--- langs/info.rkt
+++ langs/info.rkt
@@ -2,3 +2,12 @@
 (define version "1.0")
 (define collection 'multi)
 (define deps (list))
+
+;; Outlaw is omitted here because it depends on libraries that are a pain
+;; to ensure are set up properly and we don't want students to see failing
+;; tests at the beginning of the semester, nor do we want to get into
+;; setting up libraries only needed in the last week and only if you
+;; actually care to run Outlaw.
+
+;; To test outlaw you should do an explicit: raco test -c outlaw
+(define test-omit-paths (list "outlaw"))
