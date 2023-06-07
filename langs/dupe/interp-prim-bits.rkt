0266aad09e479bc9f6cda10685fdb390522089aa
David Van Horn <dvanhorn@cs.umd.edu>
2023-06-07 16:32:51 -0400

    A little clean-up.

---
+++ langs/dupe/interp-prim-bits.rkt
@@ -0,0 +1,10 @@
+#lang racket
+(require "types.rkt")
+(provide interp-prim1-bits)
+
+;; Op Bits -> Bits
+(define (interp-prim1-bits op b)
+  (match op
+    ['add1  (+ b (value->bits 1))]
+    ['sub1  (- b (value->bits 1))]
+    ['zero? (= b (value->bits 0))]))
