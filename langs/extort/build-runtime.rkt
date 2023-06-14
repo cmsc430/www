1b1d507debfbbe18b4f2843168a6a54020841244
David Van Horn <dvanhorn@cs.umd.edu>
2023-06-14 10:48:58 -0400

    Fix build-runtime for paths with spaces.

--- langs/extort/build-runtime.rkt
+++ langs/extort/build-runtime.rkt
@@ -4,9 +4,9 @@
 (require racket/runtime-path)
 (define-runtime-path here ".")
 
-(system (string-append "make -C "
+(system (string-append "make -C '"
                        (path->string (normalize-path here))
-		       " runtime.o"))
+		       "' runtime.o"))
 
 (define runtime-path
   (normalize-path (build-path here "runtime.o")))
