6a6f474d8c7a934e31090913897392d8eb914747
David Van Horn <dvanhorn@cs.umd.edu>
2023-06-22 18:10:28 -0400

    Omit src from compilation.

--- ziggy/info.rkt
+++ ziggy/info.rkt
@@ -1,6 +1,7 @@
 #lang info
 (define version "1.0")
 (define collection 'use-pkg-name)
+(define compile-omit-paths (list "src"))
 (define deps (list "base" "rackunit" "redex-lib"
                    "git+https://github.com/dvanhorn/crook.git?path=#main"))
 
