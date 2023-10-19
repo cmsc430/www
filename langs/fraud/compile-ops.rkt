e7db611cc3276d44e2b31342567bda926d5f7380
David Van Horn <dvanhorn@cs.umd.edu>
2023-10-19 08:25:20 -0400

    Add generated Fraud code.

---
+++ langs/fraud/compile-ops.rkt
@@ -0,0 +1,149 @@
+#lang racket
+(provide compile-op0 compile-op1 compile-op2 pad-stack)
+(require "ast.rkt")
+(require "types.rkt")
+(require a86/ast)
+
+(define rax 'rax)(define rdi 'rdi) ; arg
+(define r8  'r8)  ; scratch in op2
+(define r9  'r9)  ; scratch
+
+(define r15 'r15) ; stack pad (non-volatile)
+(define rsp 'rsp) ; stack
+
+;; Op0 -> Asm
+(define (compile-op0 p)
+  (match p
+    ['void      (seq (Mov rax (value->bits (void))))]
+    ['read-byte (seq pad-stack (Call 'read_byte) unpad-stack)]
+    ['peek-byte (seq pad-stack (Call 'peek_byte) unpad-stack)]))
+
+;; Op1 -> Asm
+(define (compile-op1 p)
+  (match p
+    ['add1
+                (seq (assert-integer rax)
+                     (Add rax (value->bits 1)))]
+    ['sub1
+                (seq (assert-integer rax)
+                     (Sub rax (value->bits 1)))]
+    ['zero?
+                (seq (assert-integer rax)
+                     (Cmp rax 0)
+                     if-equal)]
+    ['char?
+               (seq (And rax mask-char)
+                    (Cmp rax type-char)
+                    if-equal)]
+    ['char->integer
+               (seq (assert-char rax)
+                    (Sar rax char-shift)
+                    (Sal rax int-shift))]
+    ['integer->char
+               (seq (assert-codepoint)
+                    (Sar rax int-shift)
+                    (Sal rax char-shift)
+                    (Xor rax type-char))]
+    ['eof-object?
+               (seq (Cmp rax (value->bits eof))
+                    if-equal)]
+    ['write-byte
+               (seq assert-byte
+                    pad-stack
+                    (Mov rdi rax)
+                    (Call 'write_byte)
+                    unpad-stack)]))
+    
+
+;; Op2 -> Asm
+(define (compile-op2 p)
+  (match p
+    ['+
+     (seq (Pop r8)
+          (assert-integer r8)
+          (assert-integer rax)
+          (Add rax r8))]
+    ['-
+     (seq (Pop r8)
+          (assert-integer r8)
+          (assert-integer rax)
+          (Sub r8 rax)
+          (Mov rax r8))]
+    ['<
+     (seq (Pop r8)
+          (assert-integer r8)
+          (assert-integer rax)
+          (Cmp r8 rax)
+          if-lt)]
+    ['=
+     (seq (Pop r8)
+          (assert-integer r8)
+          (assert-integer rax)
+          (Cmp r8 rax)          
+          if-equal)]))
+
+
+;; -> Asm
+;; set rax to #t or #f if comparison flag is equal
+(define if-equal
+  (seq (Mov rax (value->bits #f))
+       (Mov r9  (value->bits #t))
+       (Cmove rax r9)))
+
+;; -> Asm
+;; set rax to #t or #f if comparison flag is less than
+(define if-lt
+  (seq (Mov rax (value->bits #f))
+       (Mov r9  (value->bits #t))
+       (Cmovl rax r9)))
+
+(define (assert-type mask type)
+  (λ (arg)
+    (seq (Mov r9 arg)
+         (And r9 mask)
+         (Cmp r9 type)
+         (Jne 'err))))
+
+(define (type-pred mask type)
+  (seq (And rax mask)
+       (Cmp rax type)
+       if-equal))
+
+(define assert-integer
+  (assert-type mask-int type-int))
+(define assert-char
+  (assert-type mask-char type-char))
+
+(define (assert-codepoint)
+  (let ((ok (gensym)))
+    (seq (assert-integer rax)
+         (Cmp rax (value->bits 0))
+         (Jl 'err)
+         (Cmp rax (value->bits 1114111))
+         (Jg 'err)
+         (Cmp rax (value->bits 55295))
+         (Jl ok)
+         (Cmp rax (value->bits 57344))
+         (Jg ok)
+         (Jmp 'err)
+         (Label ok))))
+
+(define assert-byte
+  (seq (assert-integer rax)
+       (Cmp rax (value->bits 0))
+       (Jl 'err)
+       (Cmp rax (value->bits 255))
+       (Jg 'err)))
+
+;; Asm
+;; Dynamically pad the stack to be aligned for a call
+(define pad-stack
+  (seq (Mov r15 rsp)
+       (And r15 #b1000)
+       (Sub rsp r15)))
+
+;; Asm
+;; Undo the stack alignment after a call
+(define unpad-stack
+  (seq (Add rsp r15)))
+
---
+++ langs/fraud/parse.rkt
@@ -0,0 +1,38 @@
+#lang racket
+(provide parse)
+(require "ast.rkt")
+
+;; S-Expr -> Expr
+(define (parse s)
+  (match s
+    ['eof                (Eof)]
+    [(? datum?)          (Lit s)]
+    [(? symbol?)         (Var s)]
+    [(list (? op0? o))   (Prim0 o)]
+    [(list (? op1? o) e) (Prim1 o (parse e))]
+    [(list (? op2? o) e1 e2) (Prim2 o (parse e1) (parse e2))]
+    [(list 'begin e1 e2) (Begin (parse e1) (parse e2))]
+    [(list 'if e1 e2 e3)
+     (If (parse e1) (parse e2) (parse e3))]
+    [(list 'let (list (list (? symbol? x) e1)) e2)
+     (Let x (parse e1) (parse e2))]
+    [_ (error "Parse error")]))
+
+
+;; Any -> Boolean
+(define (datum? x)
+  (or (exact-integer? x)
+      (boolean? x)
+      (char? x)))
+
+;; Any -> Boolean
+(define (op0? x)
+  (memq x '(read-byte peek-byte void)))
+
+(define (op1? x)
+  (memq x '(add1 sub1 zero? char? integer->char char->integer
+                 write-byte eof-object?)))
+
+(define (op2? x)
+  (memq x '(+ - < =)))
+
