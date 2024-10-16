56fa4257b9590d67a34f99c6ff9c90551dca7f8c
David Van Horn <dvanhorn@cs.umd.edu>
2024-10-16 17:44:31 -0400

    Nm: a86 already dependency of langs.

--- www/info.rkt
+++ www/info.rkt
@@ -3,5 +3,4 @@
                           "notes/agreement/example.rkt"))
 (define deps
   (list "base" "rackunit"
-        "git+https://github.com/cmsc430/a86.git?path=#main"
         "git+https://github.com/cmsc430/langs.git?path=#main"))
